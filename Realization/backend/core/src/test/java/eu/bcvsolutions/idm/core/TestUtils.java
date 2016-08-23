ea2dcdc338e8575e5dcaaeacfbefd06fede7c4c0
svandav <svanda.vit@seznam.cz>
2016-08-23 14:51:58 +0200

    Added test for change identity permissions - add not aprovable role #73

--- Realization/backend/core/src/test/java/eu/bcvsolutions/idm/core/TestUtils.java
+++ Realization/backend/core/src/test/java/eu/bcvsolutions/idm/core/TestUtils.java
@@ -4,10 +4,14 @@ public interface TestUtils {
 
 	public static final String HAL_CONTENT_TYPE = "application/hal+json";
 	
-	public static final String TEST_USERNAME = "admin";
+	public static final String TEST_ADMIN = "admin";
 	public static final String TEST_PASSWORD = "admin";
 	public static final String TEST_USER_1 = "testUser1";
 	public static final String TEST_USER_2 = "testUser2";
+	public static final String SUPER_ADMIN_ROLE = "superAdminRole";
+	public static final String USER_ROLE = "userRole";
+	public static final String CUSTOM_ROLE = "customRole";
+	
 	
 	
 }

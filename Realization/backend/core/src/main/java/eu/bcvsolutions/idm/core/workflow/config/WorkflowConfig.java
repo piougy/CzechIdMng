6df4bbb48b0e299206772b0024c48bba2f1f9b25
svandav <svanda.vit@seznam.cz>
2016-08-29 10:03:28 +0200

    Propagate process owner and starter to subprocesess -  added StartSubprocessEventListener for catch PROCESS_STARTED event #123

--- Realization/backend/core/src/main/java/eu/bcvsolutions/idm/core/workflow/config/WorkflowConfig.java
+++ Realization/backend/core/src/main/java/eu/bcvsolutions/idm/core/workflow/config/WorkflowConfig.java
@@ -1,62 +1,87 @@
 package eu.bcvsolutions.idm.core.workflow.config;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.delegate.event.ActivitiEventListener;
+import org.activiti.engine.delegate.event.ActivitiEventType;
 import org.activiti.engine.impl.bpmn.parser.factory.ActivityBehaviorFactory;
 import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.activiti.spring.ProcessEngineFactoryBean;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.activiti.spring.autodeployment.SingleResourceAutoDeploymentStrategy;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import eu.bcvsolutions.idm.core.workflow.domain.CustomActivityBehaviorFactory;
+import eu.bcvsolutions.idm.core.workflow.domain.StartSubprocesEventListener;
 import eu.bcvsolutions.idm.core.workflow.domain.formtype.CustomFormTypes;
+import eu.bcvsolutions.idm.notification.service.EmailService;
 
 /**
- * Workflow configuration for:
- * - custom behavior (sending email through {@link EmailService})
- * - support for custom form types (decisions etc.)
+ * Workflow configuration for: - custom behavior (sending email through
+ * {@link EmailService}) - support for custom form types (decisions etc.)
+ * - support for custom activiti event listeners
  * 
  * @author svanda, tomiska
  *
  */
 @Configuration
 public class WorkflowConfig {
-	
-	ProcessEngineConfigurationImpl processEngineConfiguration;
-	
+
+	@Autowired
+	private StartSubprocesEventListener startSubprocesEventListener;
+	//Only local variable (no autowired bean) 
+	private ProcessEngineConfigurationImpl processEngineConfiguration;
+
 	/**
 	 * Initialize custom behavior
 	 * 
 	 * @return
 	 */
-	@Bean
+	@Bean()
 	public ActivityBehaviorFactory activityBehaviorFactory() {
 		CustomActivityBehaviorFactory customActivityBehaviorFactory = new CustomActivityBehaviorFactory();
 		// Evaluate expression in workflow
-		customActivityBehaviorFactory.setExpressionManager(((SpringProcessEngineConfiguration) processEngineConfiguration).getExpressionManager());
+		customActivityBehaviorFactory.setExpressionManager(
+				((SpringProcessEngineConfiguration) processEngineConfiguration).getExpressionManager());
 		// For catch email
-		((SpringProcessEngineConfiguration) processEngineConfiguration).getBpmnParser().setActivityBehaviorFactory(customActivityBehaviorFactory);
+		((SpringProcessEngineConfiguration) processEngineConfiguration).getBpmnParser()
+				.setActivityBehaviorFactory(customActivityBehaviorFactory);
 		return customActivityBehaviorFactory;
 	}
 	
+
 	/**
-	 * * Adds suppor for custom form types
-	 * * configure single resource deployment strategy (coming soon)
+	 * * Adds support for custom form types * configure single resource
+	 * deployment strategy (coming soon) * Add custom Activiti event listeners
 	 * 
 	 * @return
 	 */
-	@Bean
+	@Bean()
 	public BeanPostProcessor activitiConfigurer() {
 		return new BeanPostProcessor() {
 
 			@Override
-			public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {				
-				if (bean instanceof ProcessEngineFactoryBean) {					
-					processEngineConfiguration = (ProcessEngineConfigurationImpl) (((ProcessEngineFactoryBean) bean).getProcessEngineConfiguration());
-					((SpringProcessEngineConfiguration) processEngineConfiguration).setDeploymentMode(SingleResourceAutoDeploymentStrategy.DEPLOYMENT_MODE);
-					processEngineConfiguration.setFormTypes(new CustomFormTypes());
+			public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof ProcessEngineFactoryBean) {
+					processEngineConfiguration = (ProcessEngineConfigurationImpl) (((ProcessEngineFactoryBean) bean)
+							.getProcessEngineConfiguration());
+					((SpringProcessEngineConfiguration) processEngineConfiguration)
+							.setDeploymentMode(SingleResourceAutoDeploymentStrategy.DEPLOYMENT_MODE);
+					((ProcessEngineConfigurationImpl)processEngineConfiguration).setFormTypes(new CustomFormTypes());
+					
+					//Add ours Activiti event listeners to engine configuration
+					addActivitiEventListeners(processEngineConfiguration);
 				}
 				return bean;
 			}
@@ -67,6 +92,16 @@ public class WorkflowConfig {
 			}
 		};
 
+	}
+
+	/**
+	 * Add ours custom Activiti event listeners to engine configuration
+	 * @param processEngineConfiguration
+	 */
+	private void addActivitiEventListeners(ProcessEngineConfigurationImpl processEngineConfiguration) {
+		Map<String, List<ActivitiEventListener>> typedListeners = new HashMap<>();
+		typedListeners.put(ActivitiEventType.PROCESS_STARTED.name(), Stream.of(startSubprocesEventListener).collect(Collectors.toList()));
+		processEngineConfiguration.setTypedEventListeners(typedListeners);
 	}
 
 

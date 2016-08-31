c97f3a96687e9878d788ec3e36394607a44c9ead
svandav <svanda.vit@seznam.cz>
2016-08-31 15:02:22 +0200

    Email notification - fix #77 and add first email notification to Change user's permissions workflow

--- Realization/backend/core/src/main/java/eu/bcvsolutions/idm/notification/service/impl/DefaultEmailService.java
+++ Realization/backend/core/src/main/java/eu/bcvsolutions/idm/notification/service/impl/DefaultEmailService.java
@@ -86,7 +86,7 @@ public class DefaultEmailService extends AbstractNotificationService implements 
 	 */
 	@Override
 	public void setEmailSent(Long emailLogId, Date sent) {
-		IdmEmailLog emailLog = emailLogRepository.findOne(emailLogId);
+		IdmEmailLog emailLog = emailLogRepository.get(emailLogId);
 		Assert.notNull(emailLog, MessageFormat.format("Email log [id:{0}] does not exist", emailLogId));
 		//
 		log.debug("Persist sent date [{}] to emailLogId [{}]", sent, emailLogId);

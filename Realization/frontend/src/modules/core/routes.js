0d2d29332ef8453e1dd0a119d304edb1f969e830
Ondrej Kopr <ondrej.kopr@bcvsolutions.eu>
2016-09-08 13:29:04 +0200

    #134 Role detail, tabs

--- Realization/frontend/src/modules/core/routes.js
+++ Realization/frontend/src/modules/core/routes.js
@@ -84,7 +84,23 @@ module.exports = {
     {
       path: 'roles',
       component: require('./content/role/Roles'),
-      access: [ { 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['ROLE_READ'] } ]
+      access: [ { 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['ROLE_READ'] } ],
+    },
+    {
+      path: 'role/:entityId/',
+      component: require('./content/role/Role'),
+      access: [ { 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['ROLE_WRITE'] } ],
+      childRoutes: [
+        {
+          path: 'detail',
+          component: require('./content/role/Content')
+        },
+      ]
+    },
+    {
+      path: 'role/:entityId/new',
+      component: require('./content/role/Content'),
+      access: [ { 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['ROLE_WRITE'] } ],
     },
     {
       path: 'tasks/:userID',

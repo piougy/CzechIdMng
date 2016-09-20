1d342ad87bc8f412629d0d0cac38877b359ae4ac
rtomiska <radek.tomiska@bcvsolutions.eu>
2016-09-20 16:23:45 +0200

    fix: useles cast in AbstractReadEntityController

--- Realization/backend/core/src/main/java/eu/bcvsolutions/idm/core/rest/impl/AbstractReadEntityController.java
+++ Realization/backend/core/src/main/java/eu/bcvsolutions/idm/core/rest/impl/AbstractReadEntityController.java
@@ -99,7 +99,7 @@ public abstract class AbstractReadEntityController<E extends BaseEntity, F exten
 			@RequestParam MultiValueMap<String, Object> parameters,
 			@PageableDefault Pageable pageable, 
 			PersistentEntityResourceAssembler assembler) {
-		return toResources((Page<Object>)findEntities(toFilter(parameters), pageable), assembler, getEntityClass(), null);
+		return toResources(findEntities(toFilter(parameters), pageable), assembler, getEntityClass(), null);
 	}
 	
 	public Page<E> findEntities(F filter, Pageable pageable) {

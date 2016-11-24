c483dbf26789c28b94bd0a7ae8290916e212da45
svandav <svanda.vit@seznam.cz>
2016-11-24 13:22:26 +0100

    ScriptArea trun on - problem was solved

--- Realization/frontend/czechidm-core/src/components/basic/ScriptArea/ScriptArea.js
+++ Realization/frontend/czechidm-core/src/components/basic/ScriptArea/ScriptArea.js
@@ -4,7 +4,6 @@ import Joi from 'joi';
 //
 import AbstractFormComponent from '../AbstractFormComponent/AbstractFormComponent';
 import Tooltip from '../Tooltip/Tooltip';
-// import AceEditor from 'react-ace';
 // import 'brace/mode/groovy';
 // import 'brace/theme/github';
 
@@ -38,67 +37,73 @@ class ScriptArea extends AbstractFormComponent {
     }
   }
 
-  // getBody(feedback) {
-  //   const { labelSpan, label, componentSpan, required, mode, helpBlock } = this.props;
-  //   //
-  //   const className = classNames('form-control');
-  //   const labelClassName = classNames(labelSpan, 'control-label');
-  //   let showAsterix = false;
-  //   if (required && !this.state.value) {
-  //     showAsterix = true;
-  //   }
-  //   const title = this.getValidationResult() != null ? this.getValidationResult().message : null;
-  //   return (
-  //     <div className={ showAsterix ? 'has-feedback' : ''}>
-  //       {
-  //         !label
-  //         ||
-  //         <label
-  //           className={labelClassName}>
-  //           {label}
-  //         </label>
-  //       }
-  //
-  //       <div className={componentSpan}>
-  //         <Tooltip ref="popover" placement="right" value={title}>
-  //           <span>
-  //             <AceEditor
-  //               ref="input"
-  //               mode={mode}
-  //               width={null}
-  //               height="10em"
-  //               className={className}
-  //               title={this.getValidationResult() != null ? this.getValidationResult().message : ''}
-  //               readOnly={this.state.readOnly}
-  //               enableBasicAutocompletion
-  //               enableLiveAutocompletion
-  //               theme="github"
-  //               onChange={this.onChange}
-  //               value={this.state.value || ''}
-  //               tabSize={4}
-  //               fontSize={14}
-  //               spellcheck
-  //               showGutter
-  //               editorProps={{$blockScrolling: true}}
-  //             />
-  //             {
-  //               feedback
-  //               ||
-  //               !showAsterix
-  //               ||
-  //               <span className="form-control-feedback" style={{color: 'red', zIndex: 0}}>*</span>
-  //             }
-  //           </span>
-  //         </Tooltip>
-  //         {
-  //           !helpBlock
-  //           ||
-  //           <span className="help-block" style={{ whiteSpace: 'normal' }}>{helpBlock}</span>
-  //         }
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  getBody(feedback) {
+    const { labelSpan, label, componentSpan, required, mode, helpBlock } = this.props;
+    //
+    const className = classNames('form-control');
+    const labelClassName = classNames(labelSpan, 'control-label');
+    let showAsterix = false;
+    if (required && !this.state.value) {
+      showAsterix = true;
+    }
+
+    // Workaround - Import for AceEditor must be here. When was on start, then not working tests (error is in AceEditor);
+    let AceEditor;
+    AceEditor = require('react-ace').default;
+    require('brace/mode/groovy');
+    require('brace/theme/github');
+    const title = this.getValidationResult() != null ? this.getValidationResult().message : null;
+    return (
+      <div className={ showAsterix ? 'has-feedback' : ''}>
+        {
+          !label
+          ||
+          <label
+            className={labelClassName}>
+            {label}
+          </label>
+        }
+
+        <div className={componentSpan}>
+          <Tooltip ref="popover" placement="right" value={title}>
+            <span>
+              <AceEditor
+                ref="input"
+                mode={mode}
+                width={null}
+                height="10em"
+                className={className}
+                title={this.getValidationResult() != null ? this.getValidationResult().message : ''}
+                readOnly={this.state.readOnly}
+                enableBasicAutocompletion
+                enableLiveAutocompletion
+                theme="github"
+                onChange={this.onChange}
+                value={this.state.value || ''}
+                tabSize={4}
+                fontSize={14}
+                spellcheck
+                showGutter
+                editorProps={{$blockScrolling: true}}
+              />
+              {
+                feedback
+                ||
+                !showAsterix
+                ||
+                <span className="form-control-feedback" style={{color: 'red', zIndex: 0}}>*</span>
+              }
+            </span>
+          </Tooltip>
+          {
+            !helpBlock
+            ||
+            <span className="help-block" style={{ whiteSpace: 'normal' }}>{helpBlock}</span>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 ScriptArea.propTypes = {

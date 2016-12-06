1cfc627891fe1934654aeb7c5ba764861dfe6f6f
rtomiska <radek.tomiska@bcvsolutions.eu>
2016-12-06 16:35:32 +0100

    #226 account management removed from core (event)

---
+++ Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/event/IdentityRoleOperationType.java
@@ -0,0 +1,11 @@
+package eu.bcvsolutions.idm.core.api.event;
+
+/**
+ * Supported identity events
+ * 
+ * @author Radek Tomiška
+ *
+ */
+public enum IdentityRoleOperationType {
+	SAVE, DELETE // TODO: split SAVE to UPDATE / CREATE?
+}

ed4e77ed932427f23aebaa21f0df2b0e098c91d5
rtomiska <radek.tomiska@bcvsolutions.eu>
2016-12-13 11:13:34 +0100

    flyway - cleanup scripts before stable version

--- Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/postgresql/V1_00_003__beryl.sql
+++
@@ -1,3 +0,0 @@
--- Html message column extend size (on TEXT) (remove previous messages)
-ALTER TABLE idm_notification DROP COLUMN html_message;
-ALTER TABLE idm_notification ADD COLUMN html_message TEXT;

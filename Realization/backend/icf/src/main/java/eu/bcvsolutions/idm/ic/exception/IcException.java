a6717ab895fbd764c91cffe36b7144dc031891e0
svandav <svanda.vit@seznam.cz>
2017-01-02 10:09:12 +0100

    Renamed ICF module to IC - exception

--- Realization/backend/icf/src/main/java/eu/bcvsolutions/idm/ic/exception/IcException.java
+++ Realization/backend/icf/src/main/java/eu/bcvsolutions/idm/ic/exception/IcException.java
@@ -1,10 +1,10 @@
-package eu.bcvsolutions.idm.icf.exception;
+package eu.bcvsolutions.idm.ic.exception;
 
 import java.util.Map;
 
 import eu.bcvsolutions.idm.core.api.exception.CoreException;
 
-public class IcfException extends CoreException {
+public class IcException extends CoreException {
 
 	/**
 	 * 
@@ -12,33 +12,33 @@ public class IcfException extends CoreException {
 	private static final long serialVersionUID = 1L;
 	
 	
-	public IcfException(String message, Map<String, Object> details, Throwable cause) {
+	public IcException(String message, Map<String, Object> details, Throwable cause) {
 		super(message, details, cause);
 	}
 
 
-	public IcfException(String message, Map<String, Object> details) {
+	public IcException(String message, Map<String, Object> details) {
 		super(message, details);
 		// TODO Auto-generated constructor stub
 	}
 
 
-	public IcfException(String message, Throwable cause) {
+	public IcException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
 
-	public IcfException(String message) {
+	public IcException(String message) {
 		super(message);
 	}
 
 
-	public IcfException(Throwable cause) {
+	public IcException(Throwable cause) {
 		super(cause);
 	}
 
 
-	public IcfException() {
+	public IcException() {
 	}
 
 }

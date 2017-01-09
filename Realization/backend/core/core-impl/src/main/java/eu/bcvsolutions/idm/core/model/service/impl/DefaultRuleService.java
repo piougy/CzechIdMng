38f9aca05b7d6a1735a2a06cde2b11cc1edc39bb
Ondrej Kopr <ondrej.kopr@bcvsolutions.eu>
2017-01-09 09:43:32 +0100

    #260 Rules add script validation

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/service/impl/DefaultRuleService.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/service/impl/DefaultRuleService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import eu.bcvsolutions.idm.core.api.repository.AbstractEntityRepository;
 import eu.bcvsolutions.idm.core.api.service.AbstractReadWriteEntityService;
+import eu.bcvsolutions.idm.core.api.service.GroovyScriptService;
 import eu.bcvsolutions.idm.core.model.dto.filter.RuleFilter;
 import eu.bcvsolutions.idm.core.model.entity.IdmRule;
 import eu.bcvsolutions.idm.core.model.service.api.IdmRuleService;
@@ -12,13 +13,26 @@ import eu.bcvsolutions.idm.core.model.service.api.IdmRuleService;
 @Service
 public class DefaultRuleService extends AbstractReadWriteEntityService<IdmRule, RuleFilter> implements IdmRuleService {
 	
+	private final GroovyScriptService groovyScriptService;
+	
 	@Autowired
-	public DefaultRuleService(AbstractEntityRepository<IdmRule, RuleFilter> repository) {
+	public DefaultRuleService(AbstractEntityRepository<IdmRule, RuleFilter> repository, GroovyScriptService groovyScriptService) {
 		super(repository);
+		
+		this.groovyScriptService = groovyScriptService;
 	}
 	
 	@Override
 	public void delete(IdmRule entity) {
 		super.delete(entity);
+	}
+	
+	@Override
+	public IdmRule save(IdmRule entity) {
+		if (entity.getScript() != null) {
+			groovyScriptService.validateScript(entity.getScript());
+		}
+		
+		return super.save(entity);
 	}
 }

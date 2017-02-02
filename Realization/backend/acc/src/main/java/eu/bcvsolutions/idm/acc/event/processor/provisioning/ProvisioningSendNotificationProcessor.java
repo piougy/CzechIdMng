042feb8e3cae070ff286bf2ff7ae68cffac17b1e
okopr <ondrej.kopr@bcvsolutions.eu>
2017-02-02 18:30:16 +0100

    #293 provisioning procesor for sending messages

---
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/event/processor/provisioning/ProvisioningSendNotificationProcessor.java
@@ -0,0 +1,76 @@
+package eu.bcvsolutions.idm.acc.event.processor.provisioning;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import com.google.common.collect.ImmutableMap;
+
+import eu.bcvsolutions.idm.acc.AccModuleDescriptor;
+import eu.bcvsolutions.idm.acc.domain.AccResultCode;
+import eu.bcvsolutions.idm.acc.domain.ProvisioningOperationType;
+import eu.bcvsolutions.idm.acc.entity.SysProvisioningOperation;
+import eu.bcvsolutions.idm.acc.service.api.SysProvisioningOperationService;
+import eu.bcvsolutions.idm.core.api.dto.DefaultResultModel;
+import eu.bcvsolutions.idm.core.api.dto.ResultModel;
+import eu.bcvsolutions.idm.core.api.event.AbstractEntityEventProcessor;
+import eu.bcvsolutions.idm.core.api.event.DefaultEventResult;
+import eu.bcvsolutions.idm.core.api.event.EntityEvent;
+import eu.bcvsolutions.idm.core.api.event.EventResult;
+import eu.bcvsolutions.idm.ic.api.IcAttribute;
+import eu.bcvsolutions.idm.ic.api.IcPasswordAttribute;
+import eu.bcvsolutions.idm.notification.entity.IdmMessage;
+import eu.bcvsolutions.idm.notification.service.api.NotificationManager;
+
+@Component
+@Description("After success provisioning send notification to identity with new generate password.")
+public class ProvisioningSendNotificationProcessor extends AbstractEntityEventProcessor<SysProvisioningOperation> {
+	
+	public static final String PROCESSOR_NAME = "provisioning-send-notification-processor";
+	private final NotificationManager notificationManager;
+	private final SysProvisioningOperationService provisioningOperationService;
+	
+	@Autowired
+	public ProvisioningSendNotificationProcessor(NotificationManager notificationManager,
+			SysProvisioningOperationService provisioningOperationService) {
+		super(ProvisioningOperationType.CREATE);
+		//
+		Assert.notNull(notificationManager);
+		Assert.notNull(provisioningOperationService);
+		//
+		this.notificationManager = notificationManager;
+		this.provisioningOperationService = provisioningOperationService;
+	}
+
+	@Override
+	public String getName() {
+		return PROCESSOR_NAME;
+	}
+
+	@Override
+	public EventResult<SysProvisioningOperation> process(EntityEvent<SysProvisioningOperation> event) {
+		SysProvisioningOperation provisioningOperation = event.getContent();
+		for (IcAttribute attribute : provisioningOperationService.getFullConnectorObject(provisioningOperation).getAttributes()) {
+			// TODO: send password always, when create?
+			if (attribute instanceof IcPasswordAttribute && attribute.getValue() != null) {
+				String password = ((IcPasswordAttribute) attribute).getPasswordValue().asString();
+				ResultModel resultModel = new DefaultResultModel(AccResultCode.PROVISIONING_NEW_PASSWORD_FOR_ACCOUNT, 
+						ImmutableMap.of("uid", provisioningOperation.getSystemEntityUid(), "system", provisioningOperation.getSystem().getName(),
+								"password", password));
+				// send message with new password to identity
+				notificationManager.send(AccModuleDescriptor.TOPIC_PROVISIONING, new IdmMessage.Builder().setModel(resultModel).build());
+				break;
+			}
+			
+		}
+		return new DefaultEventResult<>(event, this);
+	}
+	
+	@Override
+	public int getOrder() {
+		// after create
+		return 1000;
+	}
+
+}

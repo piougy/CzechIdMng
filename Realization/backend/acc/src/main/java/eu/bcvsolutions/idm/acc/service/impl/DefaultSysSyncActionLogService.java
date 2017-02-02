e20ef8254679111ebb954a022e20f99fac4b46e4
svandav <svanda.vit@seznam.cz>
2017-02-02 17:05:07 +0100

    Synchronization - implemented referential integrity for delete sync config, log , actionLog and itemLog

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/DefaultSysSyncActionLogService.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/DefaultSysSyncActionLogService.java
@@ -2,11 +2,15 @@ package eu.bcvsolutions.idm.acc.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import eu.bcvsolutions.idm.acc.dto.filter.SyncActionLogFilter;
+import eu.bcvsolutions.idm.acc.dto.filter.SyncItemLogFilter;
 import eu.bcvsolutions.idm.acc.entity.SysSyncActionLog;
 import eu.bcvsolutions.idm.acc.repository.SysSyncActionLogRepository;
 import eu.bcvsolutions.idm.acc.service.api.SysSyncActionLogService;
+import eu.bcvsolutions.idm.acc.service.api.SysSyncItemLogService;
 import eu.bcvsolutions.idm.core.api.service.AbstractReadWriteEntityService;
 
 /**
@@ -16,13 +20,33 @@ import eu.bcvsolutions.idm.core.api.service.AbstractReadWriteEntityService;
  *
  */
 @Service
-public class DefaultSysSyncActionLogService
-		extends AbstractReadWriteEntityService<SysSyncActionLog, SyncActionLogFilter>
-		implements SysSyncActionLogService {
+public class DefaultSysSyncActionLogService extends
+		AbstractReadWriteEntityService<SysSyncActionLog, SyncActionLogFilter> implements SysSyncActionLogService {
+
+	private final SysSyncItemLogService syncItemLogService;
 
 	@Autowired
-	public DefaultSysSyncActionLogService(SysSyncActionLogRepository repository) {
+	public DefaultSysSyncActionLogService(SysSyncActionLogRepository repository,
+			SysSyncItemLogService syncItemLogService) {
 		super(repository);
+		Assert.notNull(syncItemLogService);
+
+		this.syncItemLogService = syncItemLogService;
+	}
+
+	@Override
+	@Transactional
+	public void delete(SysSyncActionLog syncLog) {
+		Assert.notNull(syncLog);
+		//
+		// remove all synchronization item logs
+		SyncItemLogFilter filter = new SyncItemLogFilter();
+		filter.setSyncActionLogId(syncLog.getId());
+		syncItemLogService.find(filter, null).forEach(log -> {
+			syncItemLogService.delete(log);
+		});
+		//
+		super.delete(syncLog);
 	}
 
 }

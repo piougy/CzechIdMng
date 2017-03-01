7e0dc74ebb809ece63ad0ded4add6a8b35b7aa2f
svandav <svanda.vit@seznam.cz>
2017-03-01 17:29:17 +0100

    REST over DTO ... BaseDtoService

---
+++ Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/service/BaseDtoService.java
@@ -0,0 +1,22 @@
+package eu.bcvsolutions.idm.core.api.service;
+
+import org.springframework.plugin.core.Plugin;
+
+import eu.bcvsolutions.idm.core.api.dto.BaseDto;
+
+/**
+ * All DTO services using this interface.
+ * 
+ * @author Svanda
+ *
+ * @param <T> {@link BaseDto} type
+ */
+public interface BaseDtoService<E extends BaseDto> extends Plugin<Class<?>> {
+
+	/**
+	 * Returns {@link BaseDto} type class, which is controlled by this service
+	 * 
+	 * @return
+	 */
+	public Class<E> getDtoClass();
+}

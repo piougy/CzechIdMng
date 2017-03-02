251c2eda7b3e45884efc190b50266ca3d9fcf3f5
svandav <svanda.vit@seznam.cz>
2017-03-02 11:25:09 +0100

    REST over DTO ... converter Entity to DTO

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/config/web/WebConfig.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/config/web/WebConfig.java
@@ -1,8 +1,22 @@
 package eu.bcvsolutions.idm.core.config.web;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.commons.beanutils.PropertyUtils;
+import org.modelmapper.Condition;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.modelmapper.spi.MappingContext;
+import org.modelmapper.spi.PropertyMapping;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -27,48 +41,55 @@ import org.springframework.web.util.UrlPathHelper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
+import eu.bcvsolutions.idm.core.api.dto.AbstractDto;
+import eu.bcvsolutions.idm.core.api.entity.BaseEntity;
+import eu.bcvsolutions.idm.core.api.exception.CoreException;
 import eu.bcvsolutions.idm.core.api.rest.BaseEntityController;
 import eu.bcvsolutions.idm.core.api.rest.domain.NotExportedAssociations;
 import eu.bcvsolutions.idm.core.api.rest.domain.RequestResourceResolver;
 import eu.bcvsolutions.idm.core.config.domain.DynamicCorsConfiguration;
 import eu.bcvsolutions.idm.core.config.flyway.FlywayConfigCore;
 import eu.bcvsolutions.idm.core.exception.RestErrorAttributes;
+import eu.bcvsolutions.idm.core.model.domain.Embedded;
+import eu.bcvsolutions.idm.core.model.dto.IdmRoleDto;
 
 /**
  * Web configurations - we are reusing spring data rest web configuration
  * 
  * TODO: its not only web configuration ...
  * 
- * @author Radek Tomiška 
+ * @author Radek Tomiška
  *
  */
 @Configuration
 @AutoConfigureAfter({ FlywayConfigCore.class })
 public class WebConfig extends RepositoryRestMvcConfiguration {
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
 
 	@Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = corsConfiguration();
-        // TODO: depends on FlywayConfigCore 
-        // log.info("Starting with configurted allowed origins [{}]. Allowed origins could be changed through application setting.", config.getAllowedOrigins());
-        config.setAllowCredentials(Boolean.TRUE);
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration(BaseEntityController.BASE_PATH + "/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = corsConfiguration();
+		// TODO: depends on FlywayConfigCore
+		// log.info("Starting with configurted allowed origins [{}]. Allowed
+		// origins could be changed through application setting.",
+		// config.getAllowedOrigins());
+		config.setAllowCredentials(Boolean.TRUE);
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration(BaseEntityController.BASE_PATH + "/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
 		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return bean;
-    }
-	
+	}
+
 	/**
 	 * Cors configuration
 	 * 
@@ -78,11 +99,13 @@ public class WebConfig extends RepositoryRestMvcConfiguration {
 	public CorsConfiguration corsConfiguration() {
 		return new DynamicCorsConfiguration();
 	}
-	
+
 	/**
 	 * Whether to use suffix pattern match (".*") when matching patterns to
-	 * requests. If enabled a method mapped to "/users" also matches to "/users.*".
-	 * <p>By default this is set to {@code true}.
+	 * requests. If enabled a method mapped to "/users" also matches to
+	 * "/users.*".
+	 * <p>
+	 * By default this is set to {@code true}.
 	 * 
 	 * @see #registeredSuffixPatternMatch
 	 */
@@ -90,16 +113,16 @@ public class WebConfig extends RepositoryRestMvcConfiguration {
 	public void configurePathMatch(PathMatchConfigurer configurer) {
 		// enable encoded slash in path parameters
 		UrlPathHelper urlPathHelper = new UrlPathHelper();
-        urlPathHelper.setUrlDecode(false);
-        configurer.setUrlPathHelper(urlPathHelper);
-        //
+		urlPathHelper.setUrlDecode(false);
+		configurer.setUrlPathHelper(urlPathHelper);
+		//
 		// disable extension suffixes
 		configurer.setUseSuffixPatternMatch(Boolean.FALSE);
 		//
 		// this will be useful in future ...
 		// configurer.setUseRegisteredSuffixPatternMatch(true);
 	}
-	
+
 	/**
 	 * Common json error response attributes
 	 * 
@@ -107,12 +130,13 @@ public class WebConfig extends RepositoryRestMvcConfiguration {
 	 */
 	@Bean
 	public ErrorAttributes errorAttributes() {
-	    return new RestErrorAttributes();
+		return new RestErrorAttributes();
 	}
-	
+
 	@Bean
 	public RequestResourceResolver requestResourceResolver() {
-		return new RequestResourceResolver(defaultMessageConverters(), new DomainObjectReader(persistentEntities(), associationLinks()));
+		return new RequestResourceResolver(defaultMessageConverters(),
+				new DomainObjectReader(persistentEntities(), associationLinks()));
 	}
 
 	/**
@@ -124,7 +148,7 @@ public class WebConfig extends RepositoryRestMvcConfiguration {
 	Validator validator() {
 		return new LocalValidatorFactoryBean();
 	}
-	
+
 	/**
 	 * Adds joda time json module
 	 */
@@ -133,6 +157,101 @@ public class WebConfig extends RepositoryRestMvcConfiguration {
 		ObjectMapper mapper = super.objectMapper();
 		mapper.registerModule(new JodaModule());
 		return mapper;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modeler = new ModelMapper();
+
+		// Convert BaseEntity to UIID (get ID)
+		Converter<? extends BaseEntity, UUID> entityToUiid = new Converter<BaseEntity, UUID>() {
+
+			@Override
+			public UUID convert(MappingContext<BaseEntity, UUID> context) {
+				if (context != null && context.getSource() != null && context.getSource().getId() instanceof UUID) {
+					MappingContext<?, ?> parentContext = context.getParent();
+					if (parentContext != null && parentContext.getDestination() != null
+							&& AbstractDto.class.isAssignableFrom(parentContext.getDestinationType())
+							&& parentContext.getSource() != null
+							&& BaseEntity.class.isAssignableFrom(parentContext.getSourceType())) {
+
+						try {
+							AbstractDto parentDto = (AbstractDto) parentContext.getDestination();
+							BaseEntity entity = (BaseEntity) context.getSource();
+							Map<String, AbstractDto> embedded = parentDto.getEmbedded();
+							
+							
+							PropertyMapping propertyMapping = (PropertyMapping)context.getMapping();
+							// Find name of field by property mapping
+							String field = propertyMapping.getLastDestinationProperty().getName();
+							// Find field in DTO class
+							Field fieldTyp = parentContext.getDestinationType().getDeclaredField(field);
+							if(fieldTyp.isAnnotationPresent(Embedded.class)){
+								Embedded embeddedAnnotation = fieldTyp.getAnnotation(Embedded.class);
+								if(embeddedAnnotation.enabled()){
+									// If has field Embedded (enabled) annotation, then we will create new
+									// instance of DTO
+									AbstractDto dto = embeddedAnnotation.dtoClass().newInstance();
+									dto.setTrimmed(true);
+									// Separate map entity to new embedded DTO
+									modeler.map(entity, dto);
+									embedded.put(field, dto);
+									// Add filled DTO to embedded map to parent DTO
+									parentDto.setEmbedded(embedded);
+								}
+							}
+						} catch (InstantiationException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+							throw new CoreException(e);
+						}
+					}
+					return (UUID) context.getSource().getId();
+				}
+				return null;
+			}
+		};
+
+		// Condition for property ... if is property list and dto is trimmed,
+		// then will be not used (set null)
+		// or if is property list and have parent dto, then will be to set null
+		// (only two levels are allowed).
+		Condition<Object, Object> trimmListCondition = new Condition<Object, Object>() {
+
+			@Override
+			public boolean applies(MappingContext<Object, Object> context) {
+				if (List.class.isAssignableFrom(context.getDestinationType())) {
+					MappingContext<?, ?> parentContext = context.getParent();
+					MappingContext<?, ?> superContext = parentContext != null ? parentContext.getParent() : null;
+
+					if (superContext != null) {
+						if (parentContext != null && parentContext.getDestination() instanceof AbstractDto) {
+							((AbstractDto) parentContext).setTrimmed(true);
+						}
+						return false;
+					}
+					if (parentContext != null && parentContext.getDestination() instanceof AbstractDto
+							&& ((AbstractDto) parentContext.getDestination()).isTrimmed()) {
+						return false;
+					}
+				}
+				return true;
+			}
+
+		};
+
+		modeler.getConfiguration().setPropertyCondition(trimmListCondition);
+
+		// entity to uiid converter will be set for all entities
+		entityManager.getMetamodel().getEntities().forEach(entityType -> {
+			if (entityType.getJavaType() == null) {
+				return;
+			}
+			@SuppressWarnings("rawtypes")
+			TypeMap typeMapEntityToUiid = modeler.createTypeMap(entityType.getJavaType(), UUID.class);
+			typeMapEntityToUiid.setConverter(entityToUiid);
+		});
+
+		return modeler;
 	}
 
 	/*
@@ -168,7 +287,7 @@ public class WebConfig extends RepositoryRestMvcConfiguration {
 		validatingListener.addValidator("beforeCreate", validator);
 		validatingListener.addValidator("beforeSave", validator);
 	}
-	
+
 	/**
 	 * We want to assemble embedded object to not exported repositories too.
 	 */

9e719a775505178fadef1c58d543512fffcd24b4
svandav <svanda.vit@seznam.cz>
2017-03-03 14:48:01 +0100

    Role request - Repositories for role request and role concept

---
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/repository/IdmRoleRequestRepository.java
@@ -0,0 +1,31 @@
+package eu.bcvsolutions.idm.core.model.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import eu.bcvsolutions.idm.core.api.repository.AbstractEntityRepository;
+import eu.bcvsolutions.idm.core.model.dto.filter.RoleRequestFilter;
+import eu.bcvsolutions.idm.core.model.entity.IdmRoleRequest;
+
+/**
+ * Repository for role request
+ * @author svandav
+ *
+ */
+
+public interface IdmRoleRequestRepository extends AbstractEntityRepository<IdmRoleRequest, RoleRequestFilter> {
+	
+	/*
+	 * (non-Javadoc)
+	 * @see eu.bcvsolutions.idm.core.api.repository.BaseEntityRepository#find(eu.bcvsolutions.idm.core.api.dto.BaseFilter, Pageable)
+	 */
+	@Override
+	@Query(value = "select e from IdmRoleRequest e" +
+	        " where " +
+	        " (?#{[0].identityUUID} is null or e.identity.id = ?#{[0].identityUUID})" +
+	        " and" +
+	        " (?#{[0].state} is null or e.state = ?#{[0].state})")
+	Page<IdmRoleRequest> find(RoleRequestFilter filter, Pageable pageable);
+
+}

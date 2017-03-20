974a0b26962aebbe7ce0801a62e81b812dabdab1
svandav <svanda.vit@seznam.cz>
2017-03-20 11:21:45 +0100

    Changed type of embedded DTOs in abstract DTO (to BaseDto)

--- Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/dto/AbstractDto.java
+++ Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/dto/AbstractDto.java
@@ -48,7 +48,7 @@ public abstract class AbstractDto implements BaseDto, Auditable {
 	private UUID originalModifierId;
 	private boolean trimmed = false;
 	@JsonProperty(value = "_embedded", access=Access.READ_ONLY)
-	private Map<String, AbstractDto> embedded;
+	private Map<String, BaseDto> embedded;
 	@JsonIgnore
 	private UUID transactionId;
 	private boolean disabled;
@@ -198,14 +198,14 @@ public abstract class AbstractDto implements BaseDto, Auditable {
 		this.trimmed = trimmed;
 	}	
 
-	public Map<String, AbstractDto> getEmbedded() {
+	public Map<String, BaseDto> getEmbedded() {
 		if(embedded == null){
 			embedded = new HashMap<>();
 		}
 		return embedded;
 	}
 
-	public void setEmbedded(Map<String, AbstractDto> emmbedded) {
+	public void setEmbedded(Map<String, BaseDto> emmbedded) {
 		this.embedded = emmbedded;
 	}
 

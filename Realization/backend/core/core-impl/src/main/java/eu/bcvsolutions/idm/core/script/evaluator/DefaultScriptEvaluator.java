3f814070324238f7296ea8a672e7efc8185974f4
okopr <ondrej.kopr@bcvsolutions.eu>
2017-04-26 09:38:24 +0200

    #367 Add default git status

---
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/script/evaluator/DefaultScriptEvaluator.java
@@ -0,0 +1,39 @@
+package eu.bcvsolutions.idm.core.script.evaluator;
+
+import org.springframework.stereotype.Service;
+
+import eu.bcvsolutions.idm.core.model.domain.IdmScriptCategory;
+import eu.bcvsolutions.idm.core.model.dto.IdmScriptDto;
+
+/**
+ * Default script evaluator for {@link IdmScriptCategory} DEFAULT
+ * 
+ * @author Ondrej Kopr <kopr@xyxy.cz>
+ *
+ */
+
+@Service("defaultResourceEvaluator")
+public class DefaultResourceEvaluator extends AbstractScriptEvaluator {
+
+	@Override
+	public boolean supports(IdmScriptCategory arg0) {
+		return arg0 == IdmScriptCategory.DEFAULT;
+	}
+
+	@Override
+	public String generateTemplate(IdmScriptDto script) {
+		StringBuilder example = new StringBuilder();
+		example.append("// Inserted script: " + script.getCode() + "\n");
+		example.append("/* Description:\n");
+		example.append(script.getDescription());
+		example.append("\n");
+		example.append("*/\n");
+		example.append(SCRIPT_EVALUATOR + ".evaluate(\n");
+		example.append("    " + SCRIPT_EVALUATOR + ".newBuilder()\n");
+		example.append("        .setScriptCode('" + script.getCode() + "')\n");
+		example.append("        .addParameter('" + SCRIPT_EVALUATOR + "', " + SCRIPT_EVALUATOR + ")\n");
+		example.append("	.build());\n");
+		return example.toString();
+	}
+
+}

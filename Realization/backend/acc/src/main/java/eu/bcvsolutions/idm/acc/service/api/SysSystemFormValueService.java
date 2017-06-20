21fda54a1cbcad07afebf5777f73b2b1cbef065b
svandav <svanda.vit@seznam.cz>
2017-06-20 15:56:06 +0200

    Copy of System #515 - copy system configuration confidential values

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/api/SysSystemFormValueService.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/api/SysSystemFormValueService.java
@@ -1,5 +1,7 @@
 package eu.bcvsolutions.idm.acc.service.api;
 
+import java.util.UUID;
+
 import eu.bcvsolutions.idm.acc.entity.SysSystem;
 import eu.bcvsolutions.idm.acc.entity.SysSystemFormValue;
 import eu.bcvsolutions.idm.core.api.service.CloneableService;
@@ -11,5 +13,13 @@ import eu.bcvsolutions.idm.core.eav.service.api.FormValueService;
  *
  */
 public interface SysSystemFormValueService extends FormValueService<SysSystem, SysSystemFormValue>, CloneableService<SysSystemFormValue> {
+
+	/**
+	 * Duplicate form value. Create and persist new form value (included confidential value save)
+	 * @param id
+	 * @param owner for this value
+	 * @return
+	 */
+	SysSystemFormValue duplicate(UUID id, SysSystem owner);
 
 }

22c1ac0c10032e13abfd3a4ecaf7f93e726f9cab
Jan Helbich <janhelbich@gmail.com>
2017-07-19 16:44:05 +0200

    Automatic role addition task now supports scheduling and skipping processed items.

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/scheduler/task/impl/AbstractAutomaticRoleTaskExecutor.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/scheduler/task/impl/AbstractAutomaticRoleTaskExecutor.java
@@ -2,9 +2,9 @@ package eu.bcvsolutions.idm.core.scheduler.task.impl;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
-import eu.bcvsolutions.idm.core.api.dto.IdmRoleTreeNodeDto;
-import eu.bcvsolutions.idm.core.scheduler.service.impl.AbstractLongRunningTaskExecutor;
+import eu.bcvsolutions.idm.core.scheduler.service.impl.AbstractSchedulableTaskExecutor;
 
 /**
  * Long running task ford automatic roles
@@ -12,24 +12,24 @@ import eu.bcvsolutions.idm.core.scheduler.service.impl.AbstractLongRunningTaskEx
  * @author Radek Tomiška
  *
  */
-public abstract class AbstractAutomaticRoleTaskExecutor extends AbstractLongRunningTaskExecutor<Boolean> {
+public abstract class AbstractAutomaticRoleTaskExecutor extends AbstractSchedulableTaskExecutor<Boolean> {
 	
 	protected static final String PARAMETER_ROLE_TREE_NODE = "roleTreeNode";
 	//
-	private IdmRoleTreeNodeDto roleTreeNode = null;
+	private UUID roleTreeNode = null;
 	
-	public void setRoleTreeNode(IdmRoleTreeNodeDto roleTreeNode) {
+	public void setRoleTreeNode(UUID roleTreeNode) {
 		this.roleTreeNode = roleTreeNode;
 	}
 	
-	protected IdmRoleTreeNodeDto getRoleTreeNode() {
+	protected UUID getRoleTreeNode() {
 		return roleTreeNode;
 	}
 	
 	@Override
 	public Map<String, Object> getProperties() {
 		Map<String, Object> properties =  super.getProperties();
-		properties.put(PARAMETER_ROLE_TREE_NODE, roleTreeNode == null ? null : roleTreeNode.getId());
+		properties.put(PARAMETER_ROLE_TREE_NODE, roleTreeNode == null ? null : roleTreeNode);
 		return properties;
 	}
 	

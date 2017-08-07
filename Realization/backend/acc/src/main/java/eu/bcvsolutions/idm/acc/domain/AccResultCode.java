8f167dde288f006525fed235414b7784c2670c57
svandav <svanda.vit@seznam.cz>
2017-08-07 16:06:02 +0200

    #631 - problem with change Account identifier - Added validation on exist same role mapping (for same system)

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/domain/AccResultCode.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/domain/AccResultCode.java
@@ -87,7 +87,10 @@ public enum AccResultCode implements ResultCode {
 	AUTHENTICATION_USERNAME_DONT_EXISTS(HttpStatus.BAD_REQUEST, "Authentication failed! For username [%s] dont find username on system [%s]."),
 	AUTHENTICATION_AUTHENTICATION_ATTRIBUTE_DONT_SET(HttpStatus.BAD_REQUEST, "Authentication failed! System [%s] hasn't set authentication attribute"),
 	// Protection account system
-	ACCOUNT_CANNOT_BE_DELETED_IS_PROTECTED(HttpStatus.BAD_REQUEST, "Account [%s] cannot be deleted. Is protected before delete!");
+	ACCOUNT_CANNOT_BE_DELETED_IS_PROTECTED(HttpStatus.BAD_REQUEST, "Account [%s] cannot be deleted. Is protected before delete!"),
+	// Role system
+	ROLE_SYSTEM_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "Duplicated role mapping. Role [%s] with mapping on same system [%s] already exists!");
+	
 	
 	private final HttpStatus status;
 	private final String message;

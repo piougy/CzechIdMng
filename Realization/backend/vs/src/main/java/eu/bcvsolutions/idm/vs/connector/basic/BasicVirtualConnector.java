d8e74bf73ed7fee2ebcc53d3937107d42f7e1e00
svandav <svanda.vit@seznam.cz>
2017-08-14 16:27:43 +0200

    #639 Virtual system - added new implementation for virtual connector

--- Realization/backend/vs/src/main/java/eu/bcvsolutions/idm/vs/connector/basic/BasicVirtualConnector.java
+++ Realization/backend/vs/src/main/java/eu/bcvsolutions/idm/vs/connector/basic/BasicVirtualConnector.java
@@ -1,41 +1,15 @@
 package eu.bcvsolutions.idm.vs.connector.basic;
 
 import eu.bcvsolutions.idm.ic.api.IcConnector;
-import eu.bcvsolutions.idm.ic.api.IcConnectorClass;
 import eu.bcvsolutions.idm.ic.api.IcConnectorConfiguration;
-import eu.bcvsolutions.idm.ic.impl.IcConfigurationPropertiesImpl;
-import eu.bcvsolutions.idm.ic.impl.IcConnectorConfigurationImpl;
+import eu.bcvsolutions.idm.ic.api.annotation.IcConnectorClass;
 
-@IcConnectorClass(displayName = "Basic virtual system for CzechIdM", framework = "czechidm", name = "virtual-basic", version = "0.2.0")
+@IcConnectorClass(displayName = "Basic virtual system for CzechIdM", framework = "czechidm", name = "virtual-basic", version = "0.2.0", configurationClass = BasicVirtualConfiguration.class)
 public class BasicVirtualConnector implements IcConnector {
-
-	private IcConnectorConfiguration defaultConfiguration = null;
 
 	@Override
 	public void init(IcConnectorConfiguration configuration) {
 		// TODO Auto-generated method stub
 
 	}
-
-	@Override
-	public IcConnectorConfiguration getDefaultConfiguration() {
-		if (defaultConfiguration == null) {
-			IcConnectorConfigurationImpl config = new IcConnectorConfigurationImpl();
-			config.setConnectorPoolingSupported(false);
-			IcConfigurationPropertiesImpl configurationProperties = new IcConfigurationPropertiesImpl();
-			configurationProperties
-					.addProperty("properties", "name, firstName, lastName", String.class.getName(), "Property names",
-							"Properties for create EAV model. Values must be split by comma.", true)//
-					.addProperty("approvers", null, String.class.getName(), "Approvers",
-							"For this approvers will be created realization tasks. Every approver must be identity in CzechIdM. Value are UUIDs of identities split by comma.",
-							false);
-
-			config.setConfigurationProperties(configurationProperties);
-
-			this.defaultConfiguration = config;
-
-		}
-		return defaultConfiguration;
-	}
-
 }

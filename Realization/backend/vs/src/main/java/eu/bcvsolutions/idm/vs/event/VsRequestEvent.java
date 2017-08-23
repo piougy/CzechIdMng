93d2673f702432a0289ccb514e09cc6ef8340830
svandav <svanda.vit@seznam.cz>
2017-08-23 17:20:51 +0200

    #639 Virtual system - VS module - Added events for VsRequest

---
+++ Realization/backend/vs/src/main/java/eu/bcvsolutions/idm/vs/event/VsRequestEvent.java
@@ -0,0 +1,35 @@
+package eu.bcvsolutions.idm.vs.event;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import eu.bcvsolutions.idm.core.api.event.CoreEvent;
+import eu.bcvsolutions.idm.core.api.event.EventType;
+import eu.bcvsolutions.idm.vs.service.api.dto.VsRequestDto;
+
+/**
+ * Events for virtual system request
+ * @author svandav
+ *
+ */
+public class VsRequestEvent extends CoreEvent<VsRequestDto> {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Supported events
+	 *
+	 */
+	public enum VsRequestEventType implements EventType {
+		REALIZE_CREATE, REALIZE_UPDATE, REALIZE_DELETE, EXCECUTE
+	}
+	
+	public VsRequestEvent(VsRequestEventType operation, VsRequestDto content) {
+		super(operation, content);
+	}
+	
+	public VsRequestEvent(VsRequestEventType operation, VsRequestDto content, Map<String, Serializable> properties) {
+		super(operation, content, properties);
+	}
+
+}
---
+++ Realization/backend/vs/src/main/java/eu/bcvsolutions/idm/vs/event/processor/VsRequestRealizationProcessor.java
@@ -0,0 +1,57 @@
+package eu.bcvsolutions.idm.vs.event.processor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import eu.bcvsolutions.idm.core.api.event.CoreEventProcessor;
+import eu.bcvsolutions.idm.core.api.event.DefaultEventResult;
+import eu.bcvsolutions.idm.core.api.event.EntityEvent;
+import eu.bcvsolutions.idm.core.api.event.EventResult;
+import eu.bcvsolutions.idm.ic.api.IcUidAttribute;
+import eu.bcvsolutions.idm.vs.event.VsRequestEvent.VsRequestEventType;
+import eu.bcvsolutions.idm.vs.service.api.VsRequestService;
+import eu.bcvsolutions.idm.vs.service.api.dto.VsRequestDto;
+
+/**
+ * Realization virtual system request
+ * 
+ * @author svandav
+ *
+ */
+@Component
+@Description("Realization virtual system request")
+public class VsRequestRealizationProcessor extends CoreEventProcessor<VsRequestDto> {
+	
+	public static final String PROCESSOR_NAME = "vs-request-realization-processor";
+	public static final String RESULT_UID = "resultUidAttribute";
+	
+	private final VsRequestService service;
+	
+	@Autowired
+	public VsRequestRealizationProcessor(VsRequestService service) {
+		super(VsRequestEventType.EXCECUTE); 
+		//
+		Assert.notNull(service);
+		//
+		this.service = service;
+	}
+	
+	@Override
+	public String getName() {
+		return PROCESSOR_NAME;
+	}
+
+	@Override
+	public EventResult<VsRequestDto> process(EntityEvent<VsRequestDto> event) {
+		VsRequestDto dto = event.getContent();
+		//
+		dto = service.createRequest(dto);
+		IcUidAttribute uid = service.internalExecute(dto);
+		event.getProperties().put(VsRequestRealizationProcessor.RESULT_UID, uid);
+		event.setContent(dto);
+		//		
+		return new DefaultEventResult<>(event, this);
+	}
+}

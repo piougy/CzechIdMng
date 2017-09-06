663c7fc02cbecc7a67665bdb29ac9d063bac951c
rtomiska <radek.tomiska@bcvsolutions.eu>
2017-09-06 10:39:04 +0200

    #686 fix: confidential storage change script - rename storage key for eav attributes

--- Realization/backend/acc/src/main/resources/eu/bcvsolutions/idm/acc/sql/postgresql/V1_00_008__eav-change-confidential-storage-key.sql
+++ Realization/backend/acc/src/main/resources/eu/bcvsolutions/idm/acc/sql/postgresql/V1_00_008__eav-change-confidential-storage-key.sql
@@ -7,9 +7,8 @@
 -- system
 update idm_confidential_storage set storage_key = (
 	select 'eav:' || encode(a.id, 'hex')::uuid 
-	from idm_form_attribute a, idm_form_definition d 
-	where a.confidential = true and a.definition_id = d.id 
-		and d.definition_type = 'eu.bcvsolutions.idm.acc.entity.SysSystem' and a.code = substring(storage_key from 5)
+	from idm_form_attribute a, sys_system_form_value v
+	where v.attribute_id = a.id and idm_confidential_storage.owner_id = v.id
 )
 where storage_key like 'eav:%' and owner_type = 'eu.bcvsolutions.idm.acc.entity.SysSystemFormValue';
 
--- Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/postgresql/V1_00_028__eav-change-confidential-storage-key.sql
+++ Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/postgresql/V1_00_028__eav-change-confidential-storage-key.sql
@@ -7,33 +7,29 @@
 -- identity
 update idm_confidential_storage set storage_key = (
 	select 'eav:' || encode(a.id, 'hex')::uuid 
-	from idm_form_attribute a, idm_form_definition d 
-	where a.confidential = true and a.definition_id = d.id 
-		and d.definition_type = 'eu.bcvsolutions.idm.core.model.entity.IdmIdentity' and a.code = substring(storage_key from 5)
+	from idm_form_attribute a, idm_identity_form_value v
+	where v.attribute_id = a.id and idm_confidential_storage.owner_id = v.id
 )
 where storage_key like 'eav:%' and owner_type = 'eu.bcvsolutions.idm.core.model.entity.eav.IdmIdentityFormValue';
 -- role
 update idm_confidential_storage set storage_key = (
 	select 'eav:' || encode(a.id, 'hex')::uuid 
-	from idm_form_attribute a, idm_form_definition d 
-	where a.confidential = true and a.definition_id = d.id 
-		and d.definition_type = 'eu.bcvsolutions.idm.core.model.entity.IdmRole' and a.code = substring(storage_key from 5)
+	from idm_form_attribute a, idm_role_form_value v
+	where v.attribute_id = a.id and idm_confidential_storage.owner_id = v.id
 )
 where storage_key like 'eav:%' and owner_type = 'eu.bcvsolutions.idm.core.model.entity.eav.IdmRoleFormValue';
 -- contract
 update idm_confidential_storage set storage_key = (
 	select 'eav:' || encode(a.id, 'hex')::uuid 
-	from idm_form_attribute a, idm_form_definition d 
-	where a.confidential = true and a.definition_id = d.id 
-		and d.definition_type = 'eu.bcvsolutions.idm.core.model.entity.IdmIdentityContract' and a.code = substring(storage_key from 5)
+	from idm_form_attribute a, idm_i_contract_form_value v
+	where v.attribute_id = a.id and idm_confidential_storage.owner_id = v.id
 )
 where storage_key like 'eav:%' and owner_type = 'eu.bcvsolutions.idm.core.model.entity.eav.IdmIdentityContractFormValue';
 -- tree
 update idm_confidential_storage set storage_key = (
 	select 'eav:' || encode(a.id, 'hex')::uuid 
-	from idm_form_attribute a, idm_form_definition d 
-	where a.confidential = true and a.definition_id = d.id 
-		and d.definition_type = 'eu.bcvsolutions.idm.core.model.entity.IdmTreeNode' and a.code = substring(storage_key from 5)
+	from idm_form_attribute a, idm_tree_node_form_value v
+	where v.attribute_id = a.id and idm_confidential_storage.owner_id = v.id
 )
 where storage_key like 'eav:%' and owner_type = 'eu.bcvsolutions.idm.core.model.entity.eav.IdmTreeNodeFormValue';
 

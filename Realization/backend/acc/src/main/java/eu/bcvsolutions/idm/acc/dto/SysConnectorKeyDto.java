fcf543034086967402731d63fc5e4171135c074f
svandav <svanda.vit@seznam.cz>
2017-09-20 09:49:28 +0200

    #639 Virtual systems - Change of connector on system set allways isVirtual attribute to false (Virtual connector set this attribute on true themselves)

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/dto/SysConnectorKeyDto.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/dto/SysConnectorKeyDto.java
@@ -8,7 +8,7 @@ import eu.bcvsolutions.idm.acc.entity.SysConnectorKey;
 import eu.bcvsolutions.idm.ic.api.IcConnectorKey;
 
 /**
- * DTO for  {@link SysConnectorKey}
+ * DTO for {@link SysConnectorKey}
  * 
  * @author Ondrej Kopr <kopr@xyxy.cz>
  *
@@ -22,10 +22,10 @@ public class SysConnectorKeyDto implements IcConnectorKey, Serializable {
 	private String connectorName;
 	private String bundleName;
 	private String bundleVersion;
-	
+
 	public SysConnectorKeyDto() {
 	}
-	
+
 	public SysConnectorKeyDto(IcConnectorKey key) {
 		this.framework = key.getFramework();
 		this.connectorName = key.getConnectorName();
@@ -65,4 +65,57 @@ public class SysConnectorKeyDto implements IcConnectorKey, Serializable {
 		this.bundleVersion = bundleVersion;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bundleName == null) ? 0 : bundleName.hashCode());
+		result = prime * result + ((bundleVersion == null) ? 0 : bundleVersion.hashCode());
+		result = prime * result + ((connectorName == null) ? 0 : connectorName.hashCode());
+		result = prime * result + ((framework == null) ? 0 : framework.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SysConnectorKeyDto)) {
+			return false;
+		}
+		SysConnectorKeyDto other = (SysConnectorKeyDto) obj;
+		if (bundleName == null) {
+			if (other.bundleName != null) {
+				return false;
+			}
+		} else if (!bundleName.equals(other.bundleName)) {
+			return false;
+		}
+		if (bundleVersion == null) {
+			if (other.bundleVersion != null) {
+				return false;
+			}
+		} else if (!bundleVersion.equals(other.bundleVersion)) {
+			return false;
+		}
+		if (connectorName == null) {
+			if (other.connectorName != null) {
+				return false;
+			}
+		} else if (!connectorName.equals(other.connectorName)) {
+			return false;
+		}
+		if (framework == null) {
+			if (other.framework != null) {
+				return false;
+			}
+		} else if (!framework.equals(other.framework)) {
+			return false;
+		}
+		return true;
+	}
 }

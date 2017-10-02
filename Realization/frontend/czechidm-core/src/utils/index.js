6deb3990939c2ca88a469090105bfa1f4eaa2cf8
rtomiska <radek.tomiska@bcvsolutions.eu>
2017-10-02 11:45:13 +0200

    #575 FE external configuration - add app name, defaul pagination size

--- Realization/frontend/czechidm-core/src/utils/index.js
+++ Realization/frontend/czechidm-core/src/utils/index.js
@@ -2,12 +2,14 @@ import EntityUtils from './EntityUtils';
 import UiUtils from './UiUtils';
 import ResponseUtils from './ResponseUtils';
 import PermissionUtils from './PermissionUtils';
+import ConfigLoader from './ConfigLoader';
 
 const UtilsRoot = {
   Entity: EntityUtils,
   Ui: UiUtils,
   Response: ResponseUtils,
-  Permission: PermissionUtils
+  Permission: PermissionUtils,
+  Config: ConfigLoader
 };
 
 UtilsRoot.version = '0.0.1';

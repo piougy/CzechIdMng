ea952d37da1a77c38c86bbf1402c6bcb0efebce6
Patrik Stloukal <pastl@email.cz>
2017-10-10 18:10:44 +0200

    #744 filter test

---
+++ Realization/backend/acc/src/test/java/eu/bcvsolutions/idm/acc/service/DefaultSysRoleSystemServiceFilterTest.java
@@ -0,0 +1,99 @@
+package eu.bcvsolutions.idm.acc.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+
+import eu.bcvsolutions.idm.InitTestData;
+import eu.bcvsolutions.idm.acc.TestHelper;
+import eu.bcvsolutions.idm.acc.domain.SystemEntityType;
+import eu.bcvsolutions.idm.acc.domain.SystemOperationType;
+import eu.bcvsolutions.idm.acc.dto.SysRoleSystemDto;
+import eu.bcvsolutions.idm.acc.dto.SysSystemDto;
+import eu.bcvsolutions.idm.acc.dto.SysSystemMappingDto;
+import eu.bcvsolutions.idm.acc.dto.filter.SysRoleSystemFilter;
+import eu.bcvsolutions.idm.acc.service.api.SysRoleSystemService;
+import eu.bcvsolutions.idm.acc.service.api.SysSystemMappingService;
+import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
+import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
+
+public class DefaultSysRoleSystemServiceFilterTest extends AbstractIntegrationTest {
+	
+	@Autowired private SysRoleSystemService roleSystemService;
+	@Autowired private SysSystemMappingService mappingService;
+	@Autowired private TestHelper helper;
+	
+	@Before
+	public void login(){
+		loginAsAdmin(InitTestData.TEST_ADMIN_USERNAME);
+	}
+
+	@After
+	public void logout(){
+		super.logout();
+	}
+	
+	@Test 
+	public void testRoleIdFilter() {
+		IdmRoleDto role = helper.createRole();	
+		IdmRoleDto role2 = helper.createRole();
+		SysSystemDto system = helper.createTestResourceSystem(true);
+		SysSystemMappingDto sysSystemMappingDto = mappingService.findBySystemId(system.getId(), SystemOperationType.PROVISIONING, SystemEntityType.IDENTITY).get(0);
+		SysRoleSystemDto roleSystem = createRoleSystem(role.getId(), system.getId(), sysSystemMappingDto.getId());
+		createRoleSystem(role2.getId(), system.getId(), sysSystemMappingDto.getId());
+		SysRoleSystemFilter roleFilter = new SysRoleSystemFilter();
+		roleFilter.setRoleId(role.getId());
+		Page<SysRoleSystemDto> pages = roleSystemService.find(roleFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(roleSystem.getId(), pages.getContent().get(0).getId());
+	}
+	
+	@Test 
+	public void testSystemIdFilter() {
+		IdmRoleDto role1 = helper.createRole();	
+		IdmRoleDto role2 = helper.createRole();
+		SysSystemDto system1 = helper.createTestResourceSystem(true);
+		SysSystemDto system2 = helper.createTestResourceSystem(true);		
+		SysSystemMappingDto sysSystemMappingDto = mappingService.findBySystemId(system1.getId(), SystemOperationType.PROVISIONING, SystemEntityType.IDENTITY).get(0);		
+		SysRoleSystemDto roleSystem = createRoleSystem(role1.getId(), system1.getId(), sysSystemMappingDto.getId());
+		SysRoleSystemDto roleSystem2 = createRoleSystem(role2.getId(), system2.getId(), sysSystemMappingDto.getId());
+		SysRoleSystemFilter roleFilter = new SysRoleSystemFilter();
+		roleFilter.setSystemId(system2.getId());
+		Page<SysRoleSystemDto> pages = roleSystemService.find(roleFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(roleSystem2.getId(), pages.getContent().get(0).getId());
+	}
+	
+	@Test
+	public void testSystemMappingIdFilter() {
+		IdmRoleDto role1 = helper.createRole();	
+		IdmRoleDto role2 = helper.createRole();
+		SysSystemDto system1 = helper.createTestResourceSystem(true);
+		SysSystemDto system2 = helper.createTestResourceSystem(true);
+		SysSystemMappingDto sysSystemMappingDto1 = mappingService.findBySystemId(system1.getId(), SystemOperationType.PROVISIONING, SystemEntityType.IDENTITY).get(0);
+		SysSystemMappingDto sysSystemMappingDto2 = mappingService.findBySystemId(system2.getId(), SystemOperationType.PROVISIONING, SystemEntityType.IDENTITY).get(0);		
+		SysRoleSystemDto roleSystem1 = createRoleSystem(role1.getId(), system1.getId(), sysSystemMappingDto1.getId());
+		SysRoleSystemDto roleSystem2 = createRoleSystem(role2.getId(), system1.getId(), sysSystemMappingDto2.getId());
+		SysRoleSystemFilter roleFilter = new SysRoleSystemFilter();
+		roleFilter.setSystemMappingId(sysSystemMappingDto1.getId());
+		Page<SysRoleSystemDto> pages = roleSystemService.find(roleFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(roleSystem1.getId(), pages.getContent().get(0).getId());
+	}
+	
+	
+	private SysRoleSystemDto createRoleSystem(UUID roleId, UUID systemId, UUID systemMappingId) {
+		helper.createRole();
+		SysRoleSystemDto role = new SysRoleSystemDto();
+		role.setRole(roleId);
+		role.setSystem(systemId);
+		role.setSystemMapping(systemMappingId);
+		return roleSystemService.save(role);
+	}
+}
---
+++ Realization/backend/acc/src/test/java/eu/bcvsolutions/idm/acc/service/DefaultSysSystemServiceFilterTest.java
@@ -0,0 +1,119 @@
+package eu.bcvsolutions.idm.acc.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+
+import eu.bcvsolutions.idm.InitTestData;
+import eu.bcvsolutions.idm.acc.TestHelper;
+import eu.bcvsolutions.idm.acc.dto.SysSystemDto;
+import eu.bcvsolutions.idm.acc.dto.filter.SysSystemFilter;
+import eu.bcvsolutions.idm.acc.service.api.SysSystemService;
+import eu.bcvsolutions.idm.core.api.domain.IdmPasswordPolicyType;
+import eu.bcvsolutions.idm.core.api.dto.IdmPasswordPolicyDto;
+import eu.bcvsolutions.idm.core.api.service.IdmPasswordPolicyService;
+import eu.bcvsolutions.idm.test.api.AbstractIntegrationTest;
+
+/**
+ * 
+ * @author Patrik Stloukal
+ *
+ */
+public class DefaultSysSystemServiceFilterTest extends AbstractIntegrationTest {
+	
+	@Autowired private SysSystemService systemService;
+	@Autowired private IdmPasswordPolicyService policyService;
+	@Autowired private TestHelper helper;
+	
+	
+	@Before
+	public void login(){
+		loginAsAdmin(InitTestData.TEST_ADMIN_USERNAME);
+	}
+
+	@After
+	public void logout(){
+		super.logout();
+	}
+	
+	@Test 
+	public void testSystemNameFilter() {
+		SysSystemDto system1 = createSystem("test001" +  System.currentTimeMillis(), null);
+		createSystem("test002" +  System.currentTimeMillis(), null);
+		SysSystemFilter testFilter = new SysSystemFilter();
+		testFilter.setText(system1.getName());
+		Page<SysSystemDto> pages = systemService.find(testFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(system1.getId(), pages.getContent().get(0).getId());
+	}
+	
+	@Test 
+	public void testSystemVirtualFilter() {
+		SysSystemDto system1 = createSystem("test001" +  System.currentTimeMillis(), true);
+		createSystem("test002" +  System.currentTimeMillis(), false);
+		SysSystemFilter testFilter = new SysSystemFilter();
+		testFilter.setVirtual(true);		
+		Page<SysSystemDto> pages = systemService.find(testFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(system1.getId(), pages.getContent().get(0).getId());
+	}
+	
+	/**
+	 * Id in {@link SysSystemFilter} not currently implemented. Use get by id.
+	 */
+	@Test
+	public void testSystemIdFilter() {		
+		SysSystemDto system1 = createSystem("test001" +  System.currentTimeMillis(), null);
+		createSystem("test002" +  System.currentTimeMillis(), null);
+		SysSystemDto foundedSystem = systemService.get(system1.getId());
+		assertEquals(system1.getId(), foundedSystem.getId());
+	}
+	
+	@Test 
+	public void testSystemPasswordPolicyValidationId() {
+		SysSystemDto system1 = createSystem("test001" +  System.currentTimeMillis(), null);
+		createSystem("test002" +  System.currentTimeMillis(), null);	
+		SysSystemFilter testFilter = new SysSystemFilter();		
+		IdmPasswordPolicyDto policy = createPasswordPolicy("first", IdmPasswordPolicyType.VALIDATE);
+		system1.setPasswordPolicyValidate(policy.getId());
+		system1 = systemService.save(system1);
+		testFilter.setPasswordPolicyValidationId(policy.getId());
+		Page<SysSystemDto> pages = systemService.find(testFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(system1.getId(), pages.getContent().get(0).getId());
+	}
+	
+	@Test
+	public void testSystemPasswordPolicyGenerationId() {
+		SysSystemDto system1 = createSystem("test01" + System.currentTimeMillis(), null);
+		createSystem("test02" + System.currentTimeMillis(), null);
+		SysSystemFilter testFilter = new SysSystemFilter();
+		IdmPasswordPolicyDto policy = createPasswordPolicy("second", IdmPasswordPolicyType.GENERATE);
+		system1.setPasswordPolicyGenerate(policy.getId());
+		system1 = systemService.save(system1);
+		testFilter.setPasswordPolicyGenerationId(policy.getId());
+		Page<SysSystemDto> pages = systemService.find(testFilter, null);
+		assertEquals(1, pages.getTotalElements());
+		assertEquals(system1.getId(), pages.getContent().get(0).getId());
+	}
+	
+	private SysSystemDto createSystem(String systemName, Boolean virtual) {
+		SysSystemDto system = new SysSystemDto();
+		system.setName(systemName);
+		if (virtual != null) {
+			system.setVirtual(virtual);
+		}
+		return systemService.save(system);
+	}
+	
+	private IdmPasswordPolicyDto createPasswordPolicy(String policyName, IdmPasswordPolicyType type) {
+		IdmPasswordPolicyDto policy = new IdmPasswordPolicyDto();
+		policy.setName(policyName);
+		policy.setType(type);
+		return policyService.save(policy);
+	}
+}

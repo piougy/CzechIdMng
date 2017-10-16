9fe9152593d20527e857a201b65b1cc7982e3d03
Ondrej Kopr <ondrej.kopr@bcvsolutions.eu>
2017-10-16 13:58:17 +0200

    #761 provisioning after add or remove contract guarantee

---
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/event/ContractGuaranteeEvent.java
@@ -0,0 +1,36 @@
+package eu.bcvsolutions.idm.core.model.event;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import eu.bcvsolutions.idm.core.api.dto.IdmContractGuaranteeDto;
+import eu.bcvsolutions.idm.core.api.event.CoreEvent;
+import eu.bcvsolutions.idm.core.api.event.EventType;
+
+/**
+ * Event for {@link IdmContractGuaranteeDto} save and update.
+ * 
+ * @author Ondrej Kopr <kopr@xyxy.cz>
+ *
+ */
+
+public class ContractGuaranteeEvent extends CoreEvent<IdmContractGuaranteeDto> {
+
+	private static final long serialVersionUID = -428359305246018679L;
+
+	/**
+	 * Supported events
+	 *
+	 */
+	public enum ContractGuaranteeEventType implements EventType {
+		CREATE, UPDATE, DELETE
+	}
+	
+	public ContractGuaranteeEvent(ContractGuaranteeEventType type, IdmContractGuaranteeDto content) {
+		super(type, content);
+	}
+	
+	public ContractGuaranteeEvent(ContractGuaranteeEventType type, IdmContractGuaranteeDto content, Map<String, Serializable> properties) {
+		super(type, content, properties);
+	}
+}

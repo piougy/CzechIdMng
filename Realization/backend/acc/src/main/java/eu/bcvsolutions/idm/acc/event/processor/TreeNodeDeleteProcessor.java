0a7329751256267096c7238988050bab0ff32dff
svandav <svanda.vit@seznam.cz>
2017-10-19 13:07:10 +0200

    #741 Synchronization of relations - Integirty after delete identity (leader), tree type/node - implemented

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/event/processor/TreeNodeDeleteProcessor.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/event/processor/TreeNodeDeleteProcessor.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import eu.bcvsolutions.idm.acc.dto.filter.AccTreeAccountFilter;
+import eu.bcvsolutions.idm.acc.repository.SysSyncConfigRepository;
 import eu.bcvsolutions.idm.acc.service.api.AccTreeAccountService;
 import eu.bcvsolutions.idm.core.api.dto.IdmTreeNodeDto;
 import eu.bcvsolutions.idm.core.api.event.AbstractEntityEventProcessor;
@@ -27,14 +28,18 @@ public class TreeNodeDeleteProcessor extends AbstractEntityEventProcessor<IdmTre
 	
 	public static final String PROCESSOR_NAME = "tree-node-delete-processor";
 	private final AccTreeAccountService treeAccountService;
+	private final SysSyncConfigRepository syncConfigRepository;
 	
 	@Autowired
-	public TreeNodeDeleteProcessor(AccTreeAccountService treeAccountService) {
+	public TreeNodeDeleteProcessor(AccTreeAccountService treeAccountService,
+			SysSyncConfigRepository syncConfigRepository) {
 		super(TreeNodeEventType.DELETE);
 		//
 		Assert.notNull(treeAccountService);
+		Assert.notNull(syncConfigRepository);
 		//
 		this.treeAccountService = treeAccountService;
+		this.syncConfigRepository = syncConfigRepository;
 	}
 	
 	@Override
@@ -44,11 +49,17 @@ public class TreeNodeDeleteProcessor extends AbstractEntityEventProcessor<IdmTre
 
 	@Override
 	public EventResult<IdmTreeNodeDto> process(EntityEvent<IdmTreeNodeDto> event) {
+		IdmTreeNodeDto node = event.getContent();
 		AccTreeAccountFilter filter = new AccTreeAccountFilter();
-		filter.setTreeNodeId(event.getContent().getId());
+		filter.setTreeNodeId(node.getId());
 		treeAccountService.find(filter, null).forEach(treeAccount -> {
 			treeAccountService.delete(treeAccount);
 		});
+		
+		// Delete link to sync contract configuration
+		if(node != null && node.getId() != null) {
+			syncConfigRepository.clearDefaultTreeNode(node.getId());
+		}
 		return new DefaultEventResult<>(event, this);
 	}
 	

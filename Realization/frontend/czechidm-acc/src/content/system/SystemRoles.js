1dee40a659880cd850fb8f3ea4d728ab6df571cd
Petr Hanak <petr.nahak@gmail.com>
2017-11-20 16:19:18 +0100

    786 reused table exchanged for custom one

--- Realization/frontend/czechidm-acc/src/content/system/SystemRoles.js
+++ Realization/frontend/czechidm-acc/src/content/system/SystemRoles.js
@@ -1,50 +1,130 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
 //
-import { Basic } from 'czechidm-core';
-import { RoleManager } from 'czechidm-core/src/redux';
-import SearchParameters from 'czechidm-core/src/domain/SearchParameters';
-import RoleTable from 'czechidm-core/src/content/role/RoleTable';
+import { Basic, Advanced, Domain, Managers, Utils } from 'czechidm-core';
+import { RoleSystemManager } from '../../redux';
+import uuid from 'uuid';
 
 const uiKey = 'system-role-table';
+const manager = new RoleSystemManager();
+const roleManager = new Managers.RoleManager();
+
 /**
  * Table to display roles, assigned to system
  *
- * @author Radek Tomiška
+ * @author Petr Hanák
  */
-export default class IdentityGarantedRoles extends Basic.AbstractContent {
+class SystemRoles extends Advanced.AbstractTableContent {
 
   constructor(props, context) {
     super(props, context);
-    this.roleManager = new RoleManager();
   }
 
   getManager() {
-    return this.roleManager;
+    return manager;
+  }
+
+  getUiKey() {
+    return uiKey;
   }
 
   getContentKey() {
     return 'acc:content.systemRoles';
   }
 
-  getNavigationKey() {
-    return 'system-roles';
+  componentDidMount() {
+    this.selectNavigationItems(['sys-systems', 'system-roles']);
+  }
+
+  showDetail(add) {
+    if (add) {
+      // When we add new object class, then we need id of role as parametr and use "new" url
+      const uuidId = uuid.v1();
+      this.context.router.push(`/role/${uuidId}/new?new=1`);
+    }
   }
 
   render() {
     const { entityId } = this.props.params;
-    const forceSearchParameters = new SearchParameters().setFilter('systemId', entityId);
+    const forceSearchParameters = new Domain.SearchParameters().setFilter('systemId', entityId);
     return (
-      <div className="tab-pane-table-body">
-        { this.renderContentHeader({ style: { marginBottom: 0 }}) }
+      <div>
+        <Helmet title={this.i18n('title')} />
+        <Basic.Confirm ref="confirm-delete" level="danger"/>
+
+        <Basic.ContentHeader style={{ marginBottom: 0 }}>
+          <span dangerouslySetInnerHTML={{ __html: this.i18n('header') }}/>
+        </Basic.ContentHeader>
+
         <Basic.Panel className="no-border last">
-          <RoleTable
+          <Advanced.Table
+            ref="table"
             uiKey={uiKey}
-          roleManager={this.getManager()}
-            filterOpened={false}
-            showCatalogue={false}
-            forceSearchParameters={forceSearchParameters} />
+            manager={this.getManager()}
+            forceSearchParameters={forceSearchParameters}
+            showRowSelection={Managers.SecurityManager.hasAnyAuthority(['SYSTEM_UPDATE'])}
+            actions={
+              Managers.SecurityManager.hasAnyAuthority(['SYSTEM_UPDATE'])
+              ?
+              [{ value: 'delete', niceLabel: this.i18n('action.delete.action'), action: this.onDelete.bind(this), disabled: false }]
+              :
+              null
+            }
+            buttons={
+              [
+                <Basic.Button
+                  level="success"
+                  key="add_button"
+                  className="btn-xs"
+                  onClick={this.showDetail.bind(this, { })}
+                  rendered={Managers.SecurityManager.hasAnyAuthority('ROLE_CREATE')}>
+                  <Basic.Icon type="fa" icon="plus"/>
+                  {' '}
+                  {this.i18n('button.add')}
+                </Basic.Button>
+              ]
+            }>
+            <Advanced.ColumnLink
+              to="/role/:_target/detail"
+              target="_embedded.role.id"
+              access={{ 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['SYSTEM_READ']}}
+              property="_embedded.role.name"
+              header={this.i18n('core:entity.IdentityRole.role')}
+              sort/>
+            <Advanced.Column
+              access={{ 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['SYSTEM_READ']}}
+              property="_embedded.role.description"
+              header={this.i18n('core:entity.IdentityRole.description.title')}
+              sort/>
+            <Advanced.Column
+              face="bool"
+              access={{ 'type': 'HAS_ANY_AUTHORITY', 'authorities': ['SYSTEM_READ']}}
+              property="_embedded.role.disabled"
+              header={this.i18n('core:entity.IdentityRole.disabled.title')}
+              sort/>
+          </Advanced.Table>
         </Basic.Panel>
       </div>
     );
   }
 }
+
+SystemRoles.propTypes = {
+  role: PropTypes.object,
+  _showLoading: PropTypes.bool,
+};
+
+SystemRoles.defaultProps = {
+  role: null,
+  _showLoading: false,
+};
+
+function select(state, component) {
+  return {
+    role: Utils.Entity.getEntity(state, roleManager.getEntityType(), component.params.entityId),
+    _showLoading: Utils.Ui.isShowLoading(state, `${uiKey}-detail`),
+  };
+}
+
+export default connect(select)(SystemRoles);

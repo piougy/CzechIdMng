a42a1578dfc4c5162019c70c5c56e7b811e177b9
rtomiska <radek.tomiska@bcvsolutions.eu>
2018-01-10 17:15:37 +0100

    fix: button dynamic styles

--- Realization/frontend/czechidm-core/src/components/basic/Button/Button.js
+++ Realization/frontend/czechidm-core/src/components/basic/Button/Button.js
@@ -89,7 +89,7 @@ class Button extends AbstractComponent {
               </span>
               :
               <span>
-                <Icon value={ icon } className="icon-left"/>
+                <Icon value={ icon } className="icon-left" style={ (text || (children && children.length > 0)) ? { marginRight: 5 } : {} }/>
                 { text }
                 { children }
               </span>

7a49873034d8b7cc6fa47b6893f1f4e7ed02fbd2
svandav <svanda.vit@seznam.cz>
2018-04-18 09:40:15 +0200

    #1074 - Script sandbox problem when using custom synchronization filter

--- Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/AbstractSynchronizationExecutor.java
+++ Realization/backend/acc/src/main/java/eu/bcvsolutions/idm/acc/service/impl/AbstractSynchronizationExecutor.java
@@ -988,6 +988,7 @@ public abstract class AbstractSynchronizationExecutor<DTO extends AbstractDto>
 			allowTypes.add(IcFilterBuilder.class);
 			allowTypes.add(IcAttributeImpl.class);
 			allowTypes.add(IcAttribute.class);
+			allowTypes.add(IcNotFilter.class);
 			Object filterObj = groovyScriptService.evaluate(filterScript, variables, allowTypes);
 			if (filterObj != null && !(filterObj instanceof IcFilter)) {
 				throw new ProvisioningException(AccResultCode.SYNCHRONIZATION_FILTER_VALUE_WRONG_TYPE,

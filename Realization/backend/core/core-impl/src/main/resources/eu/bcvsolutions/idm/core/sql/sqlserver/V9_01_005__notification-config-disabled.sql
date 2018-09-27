fde9d6999ec7c2d8fdac25c130be03575a81da6f
Patrik Stloukal <pastl@email.cz>
2018-09-27 10:36:07 +0200

    1258-feedbackFix

--- Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/sqlserver/V9_01_005__notification-config-disabled.sql
+++ Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/sqlserver/V9_01_005__notification-config-disabled.sql
@@ -4,6 +4,6 @@
 --
 -- add disabled column to idm_notification_configuration
 
-ALTER TABLE idm_notification_configuration ADD COLUMN disabled boolean NOT NULL DEFAULT false;
-ALTER TABLE idm_notification_configuration_a ADD COLUMN disabled boolean;
-ALTER TABLE idm_notification_configuration_a ADD COLUMN disabled_m boolean;
+ALTER TABLE idm_notification_configuration ADD disabled bit NOT NULL;
+ALTER TABLE idm_notification_configuration_a ADD disabled bit;
+ALTER TABLE idm_notification_configuration_a ADD disabled_m bit;

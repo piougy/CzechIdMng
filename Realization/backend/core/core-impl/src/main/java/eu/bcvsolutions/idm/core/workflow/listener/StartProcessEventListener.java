49f22aca45295dce34d359ffcbbafc1251fed5d1
Vít Švanda <svanda.vit@seznam.cz>
2018-10-23 15:42:21 +0200

    Process ID is set to all processes now

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/workflow/listener/StartProcessEventListener.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/workflow/listener/StartProcessEventListener.java
@@ -14,14 +14,15 @@ import org.springframework.stereotype.Component;
 import eu.bcvsolutions.idm.core.workflow.service.WorkflowProcessInstanceService;
 
 /**
- * Listener call after process started. Add automatically implementor as starter and applicant as owner to new subprocess.
+ * Listener call after process started. Add automatically implementor as starter
+ * and applicant as owner to new subprocess.
+ * 
  * @author svandav
  *
  */
-
 @Component
-public class StartSubprocessEventListener implements ActivitiEventListener {
-	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(StartSubprocessEventListener.class);
+public class StartProcessEventListener implements ActivitiEventListener {
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(StartProcessEventListener.class);
 
 	@Autowired
 	private AutowireCapableBeanFactory beanFactory;
@@ -31,38 +32,43 @@ public class StartSubprocessEventListener implements ActivitiEventListener {
 		switch (event.getType()) {
 
 		case PROCESS_STARTED:
-			log.debug("StartSubprocesEventListener - recieve event [{}]", event.getType());
-			ActivitiProcessStartedEvent eventStarted = ((ActivitiProcessStartedEvent)event);
-			if (eventStarted.getNestedProcessInstanceId()  == null) {
+			log.debug("StartProcesEventListener - recieve event [{}]", event.getType());
+			ActivitiProcessStartedEvent eventStarted = ((ActivitiProcessStartedEvent) event);
+
+			// Manual load bean ... autowired is not possible, because this listeners are
+			// create before runtimeService
+			RuntimeService runtimeService = beanFactory.getBean(RuntimeService.class);
+			// To process set process instance ID as variable (we need use id in subprocess
+			// and sometimes in parent process too)
+			runtimeService.setVariable(event.getProcessInstanceId(), WorkflowProcessInstanceService.PROCESS_INSTANCE_ID,
+					event.getProcessInstanceId());
+
+			if (eventStarted.getNestedProcessInstanceId() == null) {
 				// Only superprocess have nested process null;
 				return;
 			}
-			// Manual load bean ... autowired is not possible, because this listeners are create before runtimeService 
-			RuntimeService runtimeService = beanFactory.getBean(RuntimeService.class);
-			
-			// To subprocess set process instance ID as variable (we need use id in subprocess)
-			runtimeService.setVariable(event.getProcessInstanceId(), WorkflowProcessInstanceService.PROCESS_INSTANCE_ID, event.getProcessInstanceId());
-			
-			@SuppressWarnings("unchecked") Map<String, Object> variables = eventStarted.getVariables();
+
+			@SuppressWarnings("unchecked")
+			Map<String, Object> variables = eventStarted.getVariables();
 			variables.forEach((k, v) -> {
 				if (WorkflowProcessInstanceService.APPLICANT_IDENTIFIER.equals(k)) {
 					String value = v == null ? null : v.toString();
 					// Set applicant as owner of process
 					runtimeService.addUserIdentityLink(event.getProcessInstanceId(), value, IdentityLinkType.OWNER);
-					log.debug("StartSubprocesEventListener - set process owner [{}]", value);
+					log.debug("StartProcesEventListener - set process owner [{}]", value);
 				} else if (WorkflowProcessInstanceService.IMPLEMENTER_IDENTIFIER.equals(k)) {
 					String value = v == null ? null : v.toString();
 					// Set current logged user (implementer) as starter of
 					// process
 					runtimeService.addUserIdentityLink(event.getProcessInstanceId(), value, IdentityLinkType.STARTER);
-					log.debug("StartSubprocesEventListener - set process starter [{}]", value);
+					log.debug("StartProcesEventListener - set process starter [{}]", value);
 				}
 			});
 
 			break;
 
 		default:
-			log.debug("StartSubprocesEventListener - receive not required event [{}]", event.getType());
+			log.debug("StartProcesEventListener - receive not required event [{}]", event.getType());
 		}
 	}
 

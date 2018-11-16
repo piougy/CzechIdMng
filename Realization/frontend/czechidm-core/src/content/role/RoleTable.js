6042cccc2b28868f48cd13f424f5e5f62a2469b4
Ondrej Kopr <ondrej.kopr@bcvsolutions.eu>
2018-11-16 12:06:30 +0100

    Remove unused import

--- Realization/frontend/czechidm-core/src/content/role/RoleTable.js
+++ Realization/frontend/czechidm-core/src/content/role/RoleTable.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import classnames from 'classnames';
 import uuid from 'uuid';
 //
 import * as Basic from '../../components/basic';

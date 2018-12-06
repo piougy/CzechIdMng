5b008b6d835fb0239e02b0dcf14da8d94229e854
Alena Peterová <alena.peterova@bcvsolutions.eu>
2018-12-06 17:33:46 +0100

    #1395 added import which was lost during merge

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/service/impl/DefaultIdmIdentityContractService.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/model/service/impl/DefaultIdmIdentityContractService.java
@@ -16,6 +16,7 @@ import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;

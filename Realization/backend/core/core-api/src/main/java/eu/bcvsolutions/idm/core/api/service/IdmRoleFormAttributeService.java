3f056e1dddbd72bd6a3e808981b69a6c2b0d325e
Vít Švanda <svanda.vit@seznam.cz>
2019-01-15 11:19:19 +0100

    #1414 - Role attribute - set sub-definition service as scriptallowed

--- Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/service/IdmRoleFormAttributeService.java
+++ Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/service/IdmRoleFormAttributeService.java
@@ -3,20 +3,21 @@ package eu.bcvsolutions.idm.core.api.service;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleFormAttributeDto;
 import eu.bcvsolutions.idm.core.api.dto.filter.IdmRoleFormAttributeFilter;
+import eu.bcvsolutions.idm.core.api.script.ScriptEnabled;
 import eu.bcvsolutions.idm.core.eav.api.dto.IdmFormAttributeDto;
 import eu.bcvsolutions.idm.core.security.api.domain.BasePermission;
 import eu.bcvsolutions.idm.core.security.api.service.AuthorizableService;
 
 /**
  * Service for relation between role and definition of form-attribution. Is elementary part
- * of role form "subdefinition".
+ * of role form "sub-definition".
  * 
  * @author Vít Švanda
  *
  */
 public interface IdmRoleFormAttributeService extends
 	EventableDtoService<IdmRoleFormAttributeDto, IdmRoleFormAttributeFilter>,
-	AuthorizableService<IdmRoleFormAttributeDto> {
+	AuthorizableService<IdmRoleFormAttributeDto>, ScriptEnabled {
 
 	/**
 	 * Add given form attribute to sub-definition of given role. 

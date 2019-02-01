6f99c5f51fd50a26e1fd93095df16a2c975802eb
Ondrej Kopr <ondrej.kopr@bcvsolutions.eu>
2019-02-01 10:28:44 +0100

    Update flyway script for sqlserver

--- Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/sqlserver/V9_04_006__form-attribute-validation.sql
+++ Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/sqlserver/V9_04_006__form-attribute-validation.sql
@@ -4,26 +4,26 @@
 --
 -- Add form attribute validation fields
 
-ALTER TABLE idm_form_attribute ADD COLUMN validation_max numeric(38,4);
-ALTER TABLE idm_form_attribute_a ADD COLUMN validation_max numeric(38,4);
-ALTER TABLE idm_form_attribute_a ADD COLUMN max_m bit;
+ALTER TABLE idm_form_attribute ADD validation_max numeric(38,4);
+ALTER TABLE idm_form_attribute_a ADD validation_max numeric(38,4);
+ALTER TABLE idm_form_attribute_a ADD max_m bit;
 
-ALTER TABLE idm_form_attribute ADD COLUMN validation_min numeric(38,4);
-ALTER TABLE idm_form_attribute_a ADD COLUMN validation_min numeric(38,4);
-ALTER TABLE idm_form_attribute_a ADD COLUMN min_m bit;
+ALTER TABLE idm_form_attribute ADD validation_min numeric(38,4);
+ALTER TABLE idm_form_attribute_a ADD validation_min numeric(38,4);
+ALTER TABLE idm_form_attribute_a ADD min_m bit;
 
-ALTER TABLE idm_form_attribute ADD COLUMN validation_regex nvarchar(2000);
-ALTER TABLE idm_form_attribute_a ADD COLUMN validation_regex nvarchar(2000);
-ALTER TABLE idm_form_attribute_a ADD COLUMN regex_m bit;
+ALTER TABLE idm_form_attribute ADD validation_regex nvarchar(2000);
+ALTER TABLE idm_form_attribute_a ADD validation_regex nvarchar(2000);
+ALTER TABLE idm_form_attribute_a ADD regex_m bit;
 
-ALTER TABLE idm_form_attribute ADD COLUMN validation_unique bit NOT NULL DEFAULT false;
-ALTER TABLE idm_form_attribute_a ADD COLUMN validation_unique bit;
-ALTER TABLE idm_form_attribute_a ADD COLUMN unique_m bit;
+ALTER TABLE idm_form_attribute ADD validation_unique bit NOT NULL DEFAULT 0;
+ALTER TABLE idm_form_attribute_a ADD validation_unique bit;
+ALTER TABLE idm_form_attribute_a ADD unique_m bit;
 
-ALTER TABLE idm_form_attribute ADD COLUMN validation_message nvarchar(2000);
-ALTER TABLE idm_form_attribute_a ADD COLUMN validation_message nvarchar(2000);
-ALTER TABLE idm_form_attribute_a ADD COLUMN validation_message_m bit;
+ALTER TABLE idm_form_attribute ADD validation_message nvarchar(2000);
+ALTER TABLE idm_form_attribute_a ADD validation_message nvarchar(2000);
+ALTER TABLE idm_form_attribute_a ADD validation_message_m bit;
 
-ALTER TABLE idm_role_form_attribute ADD COLUMN validation_message nvarchar(2000);
-ALTER TABLE idm_role_form_attribute_a ADD COLUMN validation_message nvarchar(2000);
-ALTER TABLE idm_role_form_attribute_a ADD COLUMN validation_message_m bit;
+ALTER TABLE idm_role_form_attribute ADD validation_message nvarchar(2000);
+ALTER TABLE idm_role_form_attribute_a ADD validation_message nvarchar(2000);
+ALTER TABLE idm_role_form_attribute_a ADD validation_message_m bit;

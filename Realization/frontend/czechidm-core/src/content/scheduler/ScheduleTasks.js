3549b0d07f36a06a7c82a2d30eceef74025213d6
Petr Hanak <petr.hanak@bcvsolutions.eu>
2019-03-08 14:02:15 +0100

    882 wrong cron tab component clean up

--- Realization/frontend/czechidm-core/src/content/scheduler/ScheduleTasks.js
+++ Realization/frontend/czechidm-core/src/content/scheduler/ScheduleTasks.js
@@ -277,7 +277,6 @@ class ScheduleTasks extends Advanced.AbstractTableContent {
     }));
   }
 
-
   onChangeTriggerType(triggerType) {
     this.setState({
       triggerType: triggerType.value
@@ -649,9 +648,6 @@ class ScheduleTasks extends Advanced.AbstractTableContent {
                   }
                   hidden={ triggerType !== 'CRON' }
                   required={ triggerType === 'CRON' }/>
-                <Advanced.CronTab
-                  hidden={ triggerType !== 'CRON' }
-                  required={ triggerType === 'CRON' } />
                 <Basic.SelectBox
                   ref="initiatorTaskId"
                   manager={ manager }

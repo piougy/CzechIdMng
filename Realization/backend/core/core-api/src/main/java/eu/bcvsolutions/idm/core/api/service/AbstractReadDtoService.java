3562b1c249c9cbd55df5528cbeeeea8ab701f43e
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2019-03-18 21:52:34 +0100

    #1551 fix: findIds with page request

--- Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/service/AbstractReadDtoService.java
+++ Realization/backend/core/core-api/src/main/java/eu/bcvsolutions/idm/core/api/service/AbstractReadDtoService.java
@@ -223,7 +223,8 @@ public abstract class AbstractReadDtoService<DTO extends BaseDto, E extends Base
 		
 		// count query
 		CriteriaQuery<Long> countQuery = criteriaBuilder.createQuery(Long.class);
-		countQuery.where(predicate);
+		countQuery.select(criteriaBuilder.count(countQuery.from(getEntityClass())));
+		countQuery.where(criteria.toPredicate(root, countQuery, criteriaBuilder));
 		Long total = entityManager.createQuery(countQuery).getSingleResult();
 		
 		query.setFirstResult(pageable.getOffset());

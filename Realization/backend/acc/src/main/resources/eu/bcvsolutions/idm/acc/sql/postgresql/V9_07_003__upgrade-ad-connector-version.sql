59b7efef6c0cd6bcd709a4f10c221e9b430c8992
Radek Tomiška <radek.tomiska@gmail.com>
2019-06-14 08:23:53 +0200

    #1603 Upgrade version AD connector provided in product to 1.3.4.27

---
+++ Realization/backend/acc/src/main/resources/eu/bcvsolutions/idm/acc/sql/postgresql/V9_07_003__upgrade-ad-connector-version.sql
@@ -0,0 +1,18 @@
+--
+-- CzechIdM 9 Flyway script 
+-- BCV solutions s.r.o.
+--
+-- update product provided AD connector
+
+-- update connector definition
+UPDATE idm_form_definition SET code = 'connId:net.tirasa.connid.bundles.ad.ADConnector:net.tirasa.connid.bundles.ad:1.3.4.27', name = 'connId:net.tirasa.connid.bundles.ad.ADConnector:net.tirasa.connid.bundles.ad:1.3.4.27' 
+WHERE code = 'connId:net.tirasa.connid.bundles.ad.ADConnector:net.tirasa.connid.bundles.ad:1.3.4.25'
+AND NOT EXISTS (SELECT 1 FROM idm_form_definition WHERE code = 'connId:net.tirasa.connid.bundles.ad.ADConnector:net.tirasa.connid.bundles.ad:1.3.4.27');
+
+-- set new connector version for locally (~prosuct) configured systems 
+UPDATE sys_system SET connector_bundle_version = '1.3.4.27' 
+WHERE connector_bundle_version = '1.3.4.25' 
+AND connector_name = 'net.tirasa.connid.bundles.ad.ADConnector'
+AND connector_bundle_name = 'net.tirasa.connid.bundles.ad'
+AND connector_framework = 'connId'
+AND remote = false;

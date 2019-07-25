c08571cdbf8eab4162dd61003d1fccc9d92a9809
Radek Tomiška <radek.tomiska@gmail.com>
2019-07-25 10:31:54 +0200

    fix mssql change scripts, remove deprecated gui module

--- Realization/backend/acc/src/main/resources/eu/bcvsolutions/idm/acc/sql/sqlserver/V9_07_006__assign-role-to-all-contracts.sql
+++ Realization/backend/acc/src/main/resources/eu/bcvsolutions/idm/acc/sql/sqlserver/V9_07_006__assign-role-to-all-contracts.sql
@@ -4,6 +4,6 @@
 --
 -- Sync option: Assign default role to all valid or future valid contracts
 
-ALTER TABLE sys_sync_identity_config ADD COLUMN assign_default_role_to_all bit NOT NULL DEFAULT 0;
-ALTER TABLE sys_sync_identity_config_a ADD COLUMN assign_default_role_to_all bit;
-ALTER TABLE sys_sync_identity_config_a ADD COLUMN assign_default_role_to_all_m bit;
+ALTER TABLE sys_sync_identity_config ADD assign_default_role_to_all bit NOT NULL DEFAULT 0;
+ALTER TABLE sys_sync_identity_config_a ADD assign_default_role_to_all bit;
+ALTER TABLE sys_sync_identity_config_a ADD assign_default_role_to_all_m bit;

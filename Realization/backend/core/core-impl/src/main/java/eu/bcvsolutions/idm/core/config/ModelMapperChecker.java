ea18e114aac3372470e32c9c8354cba56e3f781a
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2019-10-03 11:15:37 +0200

    #1888 exclude historic wf services from model mapper check (takes too long)

--- Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/config/ModelMapperChecker.java
+++ Realization/backend/core/core-impl/src/main/java/eu/bcvsolutions/idm/core/config/ModelMapperChecker.java
@@ -16,6 +16,8 @@ import eu.bcvsolutions.idm.core.api.dto.filter.BaseFilter;
 import eu.bcvsolutions.idm.core.api.dto.filter.DataFilter;
 import eu.bcvsolutions.idm.core.api.exception.CoreException;
 import eu.bcvsolutions.idm.core.api.service.ReadDtoService;
+import eu.bcvsolutions.idm.core.workflow.service.impl.DefaultWorkflowHistoricProcessInstanceService;
+import eu.bcvsolutions.idm.core.workflow.service.impl.DefaultWorkflowHistoricTaskInstanceService;
 
 /**
  * Check model mapper is properly initialized to prevent:
@@ -43,6 +45,14 @@ public class ModelMapperChecker {
 		int modelMapperUsed = 0;
 		Map<String, ReadDtoService> services = context.getBeansOfType(ReadDtoService.class);
 		for (ReadDtoService service : services.values()) {
+			if ((service instanceof DefaultWorkflowHistoricTaskInstanceService)
+					|| (service instanceof DefaultWorkflowHistoricProcessInstanceService)) {
+				LOG.debug("Workflow history service [{}] will not be checked - find method with pagination is not fully supported and took long time."
+						, service.getClass());
+				continue;
+			}
+			//
+			LOG.trace("Service [{}] will be checked.", service.getClass());
 			try {
 				BaseFilter filter = null;
 				if (service.getFilterClass() != null) { // some services could not define filter
@@ -61,7 +71,7 @@ public class ModelMapperChecker {
 					modelMapperUsed++;
 				}
 			} catch (UnsupportedOperationException ex) {
-				LOG.debug("Service [{}] does not support find method. Check will be skipped.", service.getClass());
+				LOG.warn("Service [{}] does not support find method. Check will be skipped.", service.getClass());
 			} catch (MappingException ex) {
 				// Throw exception => prevent to IdM starts in invalid state.
 				throw new CoreException(
@@ -70,6 +80,7 @@ public class ModelMapperChecker {
 			} catch (Exception ex) {
 				LOG.error("Service [{}] cannot be checked. Find method cannot be called.", service.getClass(), ex);
 			}
+			LOG.trace("Service [{}] was checked.", service.getClass());
 		}
 		LOG.info("Init: all registered IdM services [{}]. "
 				+ "Services usage were checked [{}] (agenda contains some records) [took: {}ms]."

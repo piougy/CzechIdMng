d0257145192ebab3781305845e47b12157ee63c8
ohusnik <57331664+ohusnik@users.noreply.github.com>
2019-11-14 09:37:43 +0100

    #1936 Sync actions - turn off log delete option (#62)

--- Realization/frontend/czechidm-acc/src/content/system/SystemSynchronizationLogDetail.js
+++ Realization/frontend/czechidm-acc/src/content/system/SystemSynchronizationLogDetail.js
@@ -118,13 +118,7 @@ class SystemSynchronizationLogDetail extends Advanced.AbstractTableContent {
             manager={syncActionLogManager}
             forceSearchParameters={forceSearchParameters}
             showRowSelection={Managers.SecurityManager.hasAnyAuthority(['SYSTEM_UPDATE'])}
-            actions={
-              Managers.SecurityManager.hasAnyAuthority(['SYSTEM_UPDATE'])
-              ?
-              [{ value: 'delete', niceLabel: this.i18n('action.delete.action'), action: this.onDelete.bind(this), disabled: false }]
-              :
-              null
-            }>
+          >
             <Advanced.Column
               property=""
               header=""

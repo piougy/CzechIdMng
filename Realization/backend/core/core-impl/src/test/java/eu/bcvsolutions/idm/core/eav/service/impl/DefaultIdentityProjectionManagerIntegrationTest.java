c60134b83d9d3aefa5b93090c4bc5c00a1606831
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2020-06-05 10:01:35 +0200

    test: try to stable DefaultIdentityProjectionManagerIntegrationTest

--- Realization/backend/core/core-impl/src/test/java/eu/bcvsolutions/idm/core/eav/service/impl/DefaultIdentityProjectionManagerIntegrationTest.java
+++ Realization/backend/core/core-impl/src/test/java/eu/bcvsolutions/idm/core/eav/service/impl/DefaultIdentityProjectionManagerIntegrationTest.java
@@ -23,15 +23,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.bcvsolutions.idm.core.api.domain.ConfigurationMap;
 import eu.bcvsolutions.idm.core.api.domain.PriorityType;
+import eu.bcvsolutions.idm.core.api.domain.RoleRequestState;
 import eu.bcvsolutions.idm.core.api.dto.IdmContractPositionDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityContractDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmIdentityRoleDto;
 import eu.bcvsolutions.idm.core.api.dto.IdmRoleDto;
+import eu.bcvsolutions.idm.core.api.dto.IdmRoleRequestDto;
+import eu.bcvsolutions.idm.core.api.dto.filter.IdmRoleRequestFilter;
 import eu.bcvsolutions.idm.core.api.dto.projection.IdmIdentityProjectionDto;
 import eu.bcvsolutions.idm.core.api.exception.ForbiddenEntityException;
 import eu.bcvsolutions.idm.core.api.rest.BaseController;
+import eu.bcvsolutions.idm.core.api.service.IdmIdentityRoleService;
 import eu.bcvsolutions.idm.core.api.service.IdmIdentityService;
+import eu.bcvsolutions.idm.core.api.service.IdmRoleRequestService;
 import eu.bcvsolutions.idm.core.api.service.LookupService;
 import eu.bcvsolutions.idm.core.eav.api.domain.PersistentType;
 import eu.bcvsolutions.idm.core.eav.api.dto.FormDefinitionAttributes;
@@ -69,6 +74,8 @@ public class DefaultIdentityProjectionManagerIntegrationTest extends AbstractRes
 	@Autowired private IdmFormProjectionService projectionService;
 	@Autowired private LookupService lookupService;
 	@Autowired private IdmIdentityService identityService;
+	@Autowired private IdmIdentityRoleService identityRoleService;
+	@Autowired private IdmRoleRequestService roleRequestService;
 	//
 	private DefaultIdentityProjectionManager manager;
 
@@ -258,6 +265,24 @@ public class DefaultIdentityProjectionManagerIntegrationTest extends AbstractRes
 						&& p.getIdentityContract().equals(positionTwo.getIdentityContract());
 			}));
 			// assigned roles
+			// check directly by service
+			IdmRoleRequestFilter roleRequestFilter = new IdmRoleRequestFilter();
+			roleRequestFilter.setApplicantId(createdProjection.getIdentity().getId());
+			List<IdmRoleRequestDto> roleRequests = roleRequestService.find(roleRequestFilter, null).getContent();
+			Assert.assertFalse(roleRequests.isEmpty());
+			Assert.assertTrue(roleRequests.stream().allMatch(r -> r.getState() == RoleRequestState.EXECUTED));
+			List<IdmIdentityRoleDto> assignedRoles = identityRoleService.findAllByIdentity(createdProjection.getIdentity().getId());
+			Assert.assertEquals(2, assignedRoles.size());
+			Assert.assertTrue(assignedRoles.stream().anyMatch(ir -> {
+				return ir.getRole().equals(roleOne.getId())
+						&& ir.getValidFrom().equals(identityRoleOne.getValidFrom())
+						&& ir.getIdentityContract().equals(createdPrimeContract.getId());
+			}));
+			Assert.assertTrue(assignedRoles.stream().anyMatch(ir -> {
+				return ir.getRole().equals(roleTwo.getId())
+						&& ir.getIdentityContract().equals(otherContractTwo.getId());
+			}));
+			// check by projection
 			Assert.assertEquals(2, createdProjection.getIdentityRoles().size());
 			Assert.assertTrue(createdProjection.getIdentityRoles().stream().anyMatch(ir -> {
 				return ir.getRole().equals(roleOne.getId())

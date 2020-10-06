7140b6b2333347cb73fed9c77ce2424a9e6db18b
Radek Tomiška <radek.tomiska@bcvsolutions.eu>
2020-10-06 08:28:46 +0200

    #1142 mssql change script

--- Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/sqlserver/V10_06_004__entity-event-start-end.sql
+++ Realization/backend/core/core-impl/src/main/resources/eu/bcvsolutions/idm/core/sql/sqlserver/V10_06_004__entity-event-start-end.sql
@@ -4,8 +4,8 @@
 --
 -- entity event start and end date
 
-ALTER TABLE idm_entity_event ADD COLUMN event_started datetime2(6);
-ALTER TABLE idm_entity_event ADD COLUMN event_ended datetime2(6);
+ALTER TABLE idm_entity_event ADD event_started datetime2(6);
+ALTER TABLE idm_entity_event ADD event_ended datetime2(6);
 
 UPDATE idm_entity_event SET event_started = created;
 UPDATE idm_entity_event SET event_ended = modified WHERE result_state <> 'CREATED' AND result_state <> 'RUNNING';
